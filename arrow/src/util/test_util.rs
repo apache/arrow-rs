6f33ba245092d03dd6120e6e97f39ff16b4afc46
Jorge C. Leitao <jorgecarleitao@gmail.com>
2021-04-18 17:35:17 +0000

    Fixed Linting.

--- arrow/src/util/test_util.rs
+++ arrow/src/util/test_util.rs
@@ -98,10 +98,7 @@ pub fn arrow_test_data() -> String {
 /// assert!(std::path::PathBuf::from(filename).exists());
 /// ```
 pub fn parquet_test_data() -> String {
-    match get_data_dir(
-        "PARQUET_TEST_DATA",
-        "../parquet-testing/data",
-    ) {
+    match get_data_dir("PARQUET_TEST_DATA", "../parquet-testing/data") {
         Ok(pb) => pb.display().to_string(),
         Err(err) => panic!("failed to get parquet data dir: {}", err),
     }
