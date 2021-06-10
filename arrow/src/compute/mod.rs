a7656a8a3cd1f02e4543e1b971842ca92404f82a
Andrew Lamb <andrew@nerdnetworks.org>
2021-06-10 10:58:36 -0400

    refactor lexico sort (#424) (#441)
    Co-authored-by: Jiayu Liu <Jimexist@users.noreply.github.com>

--- arrow/src/compute/mod.rs
+++ arrow/src/compute/mod.rs
@@ -30,6 +30,7 @@ pub use self::kernels::comparison::*;
 pub use self::kernels::concat::*;
 pub use self::kernels::filter::*;
 pub use self::kernels::limit::*;
+pub use self::kernels::partition::*;
 pub use self::kernels::regexp::*;
 pub use self::kernels::sort::*;
 pub use self::kernels::take::*;
