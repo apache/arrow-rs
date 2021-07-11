427ca9ee3ca70823b73ca4a8a0117fdbe15fcd03
Neville Dipale <nevilledips@gmail.com>
2021-07-11 11:50:09 +0200

    Map array tests

--- arrow/src/array/array_map.rs
+++ arrow/src/array/array_map.rs
@@ -65,10 +65,9 @@ impl MapArray {
 
     /// Returns ith value of this map array.
     pub fn value(&self, i: usize) -> ArrayRef {
-        let end = self.value_offsets()[i + 1];
-        let start = self.value_offsets()[i];
-        self.values
-            .slice(start.to_usize().unwrap(), (end - start).to_usize().unwrap())
+        let end = self.value_offsets()[i + 1] as usize;
+        let start = self.value_offsets()[i] as usize;
+        self.values.slice(start, end - start)
     }
 
     /// Returns the offset values in the offsets buffer
@@ -115,9 +114,9 @@ impl MapArray {
             )));
         }
 
-        let values = data.child_data()[0].clone();
+        let entries = data.child_data()[0].clone();
 
-        if let DataType::Struct(fields) = values.data_type() {
+        if let DataType::Struct(fields) = entries.data_type() {
             if fields.len() != 2 {
                 return Err(ArrowError::InvalidArgumentError(format!(
                 "MapArray should contain a struct array with 2 fields, have {} fields",
@@ -127,11 +126,11 @@ impl MapArray {
         } else {
             return Err(ArrowError::InvalidArgumentError(format!(
                 "MapArray should contain a struct array child, found {:?}",
-                values.data_type()
+                entries.data_type()
             )));
         }
 
-        let values = make_array(values);
+        let values = make_array(entries);
         let value_offsets = data.buffers()[0].as_ptr();
 
         let value_offsets = unsafe { RawPtrBox::<i32>::new(value_offsets) };
@@ -180,272 +179,187 @@ impl fmt::Debug for MapArray {
     }
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use crate::{
-//         array::ArrayData,
-//         array::{Int32Array, StructArray},
-//         buffer::Buffer,
-//         datatypes::Field,
-//         datatypes::ToByteSlice,
-//         util::bit_util,
-//     };
+#[cfg(test)]
+mod tests {
+    use std::sync::Arc;
 
-//     use super::*;
+    use crate::{
+        array::ArrayData,
+        array::{Int32Array, StructArray, UInt32Array},
+        buffer::Buffer,
+        datatypes::Field,
+        datatypes::ToByteSlice,
+    };
 
-//     fn create_from_buffers() -> MapArray {
-//         // Construct key and values
-//         let keys_data = ArrayData::builder(DataType::Int32)
-//             .len(8)
-//             .add_buffer(Buffer::from(&[0, 1, 2, 3, 4, 5, 6, 7].to_byte_slice()))
-//             .build();
-//         let values_data = ArrayData::builder(DataType::UInt32)
-//             .len(8)
-//             .add_buffer(Buffer::from(&[0u32, 1, 2, 3, 4, 5, 6, 7].to_byte_slice()))
-//             .build();
+    use super::*;
 
-//         // Construct a buffer for value offsets, for the nested array:
-//         //  [[0, 1, 2], [3, 4, 5], [6, 7]]
-//         let entry_offsets = Buffer::from(&[0, 3, 6, 8].to_byte_slice());
+    fn create_from_buffers() -> MapArray {
+        // Construct key and values
+        let keys_data = ArrayData::builder(DataType::Int32)
+            .len(8)
+            .add_buffer(Buffer::from(&[0, 1, 2, 3, 4, 5, 6, 7].to_byte_slice()))
+            .build();
+        let values_data = ArrayData::builder(DataType::UInt32)
+            .len(8)
+            .add_buffer(Buffer::from(&[0u32, 1, 2, 3, 4, 5, 6, 7].to_byte_slice()))
+            .build();
 
-//         let keys = Field::new("keys", DataType::Int32, false);
-//         let values = Field::new("values", DataType::UInt32, false);
-//         let entry_struct = StructArray::from(vec![
-//             (keys, make_array(keys_data)),
-//             (values, make_array(values_data)),
-//         ]);
+        // Construct a buffer for value offsets, for the nested array:
+        //  [[0, 1, 2], [3, 4, 5], [6, 7]]
+        let entry_offsets = Buffer::from(&[0, 3, 6, 8].to_byte_slice());
 
-//         // Construct a map array from the above two
-//         let map_data_type = DataType::Map(
-//             Box::new(Field::new(
-//                 "entries",
-//                 entry_struct.data_type().clone(),
-//                 true,
-//             )),
-//             false,
-//         );
-//         let map_data = ArrayData::builder(map_data_type)
-//             .len(3)
-//             .add_buffer(entry_offsets)
-//             .add_child_data(entry_struct.data().clone())
-//             .build();
-//         MapArray::from(map_data)
-//     }
+        let keys = Field::new("keys", DataType::Int32, false);
+        let values = Field::new("values", DataType::UInt32, false);
+        let entry_struct = StructArray::from(vec![
+            (keys, make_array(keys_data)),
+            (values, make_array(values_data)),
+        ]);
 
-//     #[test]
-//     fn test_map_array() {
-//         let map_array = create_from_buffers();
+        // Construct a map array from the above two
+        let map_data_type = DataType::Map(
+            Box::new(Field::new(
+                "entries",
+                entry_struct.data_type().clone(),
+                true,
+            )),
+            false,
+        );
+        let map_data = ArrayData::builder(map_data_type)
+            .len(3)
+            .add_buffer(entry_offsets)
+            .add_child_data(entry_struct.data().clone())
+            .build();
+        MapArray::from(map_data)
+    }
 
-//         let values = map_array.values();
-//         assert_eq!(&value_data, values.data());
-//         assert_eq!(DataType::Int32, map_array.value_type());
-//         assert_eq!(3, map_array.len());
-//         assert_eq!(0, map_array.null_count());
-//         assert_eq!(6, map_array.value_offsets()[2]);
-//         assert_eq!(2, map_array.value_length(2));
-//         assert_eq!(
-//             0,
-//             map_array
-//                 .value(0)
-//                 .as_any()
-//                 .downcast_ref::<Int32Array>()
-//                 .unwrap()
-//                 .value(0)
-//         );
-//         assert_eq!(
-//             0,
-//             unsafe { map_array.value_unchecked(0) }
-//                 .as_any()
-//                 .downcast_ref::<Int32Array>()
-//                 .unwrap()
-//                 .value(0)
-//         );
-//         for i in 0..3 {
-//             assert!(map_array.is_valid(i));
-//             assert!(!map_array.is_null(i));
-//         }
+    #[test]
+    fn test_map_array() {
+        // Construct key and values
+        let key_data = ArrayData::builder(DataType::Int32)
+            .len(8)
+            .add_buffer(Buffer::from(&[0, 1, 2, 3, 4, 5, 6, 7].to_byte_slice()))
+            .build();
+        let value_data = ArrayData::builder(DataType::UInt32)
+            .len(8)
+            .add_buffer(Buffer::from(&[0u32, 1, 2, 3, 4, 5, 6, 7].to_byte_slice()))
+            .build();
 
-//         // Now test with a non-zero offset
-//         let map_data = ArrayData::builder(map_array.data_type().clone())
-//             .len(3)
-//             .offset(1)
-//             .add_buffer(map_array.data().buffers()[0].clone())
-//             .add_child_data(map_array.data().child_data()[0].clone())
-//             .build();
-//         let map_array = MapArray::from(map_data);
+        // Construct a buffer for value offsets, for the nested array:
+        //  [[0, 1, 2], [3, 4, 5], [6, 7]]
+        let entry_offsets = Buffer::from(&[0, 3, 6, 8].to_byte_slice());
 
-//         let values = map_array.values();
-//         // assert_eq!(&value_data, values.data());
-//         assert_eq!(DataType::Int32, map_array.value_type());
-//         assert_eq!(3, map_array.len());
-//         assert_eq!(0, map_array.null_count());
-//         assert_eq!(6, map_array.value_offsets()[1]);
-//         assert_eq!(2, map_array.value_length(1));
-//         assert_eq!(
-//             3,
-//             map_array
-//                 .value(0)
-//                 .as_any()
-//                 .downcast_ref::<StructArray>()
-//                 .unwrap()
-//                 .value(0)
-//         );
-//         assert_eq!(
-//             3,
-//             unsafe { map_array.value_unchecked(0) }
-//                 .as_any()
-//                 .downcast_ref::<Int32Array>()
-//                 .unwrap()
-//                 .value(0)
-//         );
-//     }
+        let keys_field = Field::new("keys", DataType::Int32, false);
+        let values_field = Field::new("values", DataType::UInt32, false);
+        let entry_struct = StructArray::from(vec![
+            (keys_field.clone(), make_array(key_data)),
+            (values_field.clone(), make_array(value_data.clone())),
+        ]);
 
-//     #[test]
-//     fn test_map_array_slice() {
-//         // Construct a value array
-//         let value_data = ArrayData::builder(DataType::Int32)
-//             .len(10)
-//             .add_buffer(Buffer::from_slice_ref(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
-//             .build();
+        // Construct a map array from the above two
+        let map_data_type = DataType::Map(
+            Box::new(Field::new(
+                "entries",
+                entry_struct.data_type().clone(),
+                true,
+            )),
+            false,
+        );
+        let map_data = ArrayData::builder(map_data_type)
+            .len(3)
+            .add_buffer(entry_offsets)
+            .add_child_data(entry_struct.data().clone())
+            .build();
+        let map_array = MapArray::from(map_data);
 
-//         // Construct a buffer for value offsets, for the nested array:
-//         //  [[0, 1], null, null, [2, 3], [4, 5], null, [6, 7, 8], null, [9]]
-//         let value_offsets = Buffer::from_slice_ref(&[0, 2, 2, 2, 4, 6, 6, 9, 9, 10]);
-//         // 01011001 00000001
-//         let mut null_bits: [u8; 2] = [0; 2];
-//         bit_util::set_bit(&mut null_bits, 0);
-//         bit_util::set_bit(&mut null_bits, 3);
-//         bit_util::set_bit(&mut null_bits, 4);
-//         bit_util::set_bit(&mut null_bits, 6);
-//         bit_util::set_bit(&mut null_bits, 8);
+        let values = map_array.values();
+        assert_eq!(&value_data, values.data());
+        assert_eq!(DataType::UInt32, map_array.value_type());
+        assert_eq!(3, map_array.len());
+        assert_eq!(0, map_array.null_count());
+        assert_eq!(6, map_array.value_offsets()[2]);
+        assert_eq!(2, map_array.value_length(2));
 
-//         // Construct a map array from the above two
-//         let list_data_type =
-//             DataType::List(Box::new(Field::new("item", DataType::Int32, false)));
-//         let list_data = ArrayData::builder(list_data_type)
-//             .len(9)
-//             .add_buffer(value_offsets)
-//             .add_child_data(value_data.clone())
-//             .null_bit_buffer(Buffer::from(null_bits))
-//             .build();
-//         let list_array = MapArray::from(list_data);
+        let key_array = Arc::new(Int32Array::from(vec![0, 1, 2])) as ArrayRef;
+        let value_array = Arc::new(UInt32Array::from(vec![0, 1, 2])) as ArrayRef;
+        let struct_array = StructArray::from(vec![
+            (keys_field.clone(), key_array),
+            (values_field.clone(), value_array),
+        ]);
+        assert_eq!(
+            struct_array,
+            StructArray::from(map_array.value(0).data().clone())
+        );
+        assert_eq!(
+            &struct_array,
+            unsafe { map_array.value_unchecked(0) }
+                .as_any()
+                .downcast_ref::<StructArray>()
+                .unwrap()
+        );
+        for i in 0..3 {
+            assert!(map_array.is_valid(i));
+            assert!(!map_array.is_null(i));
+        }
 
-//         let values = list_array.values();
-//         assert_eq!(&value_data, values.data());
-//         assert_eq!(DataType::Int32, list_array.value_type());
-//         assert_eq!(9, list_array.len());
-//         assert_eq!(4, list_array.null_count());
-//         assert_eq!(2, list_array.value_offsets()[3]);
-//         assert_eq!(2, list_array.value_length(3));
+        // Now test with a non-zero offset
+        let map_data = ArrayData::builder(map_array.data_type().clone())
+            .len(3)
+            .offset(1)
+            .add_buffer(map_array.data().buffers()[0].clone())
+            .add_child_data(map_array.data().child_data()[0].clone())
+            .build();
+        let map_array = MapArray::from(map_data);
 
-//         let sliced_array = list_array.slice(1, 6);
-//         assert_eq!(6, sliced_array.len());
-//         assert_eq!(1, sliced_array.offset());
-//         assert_eq!(3, sliced_array.null_count());
+        let values = map_array.values();
+        assert_eq!(&value_data, values.data());
+        assert_eq!(DataType::UInt32, map_array.value_type());
+        assert_eq!(3, map_array.len());
+        assert_eq!(0, map_array.null_count());
+        assert_eq!(6, map_array.value_offsets()[1]);
+        assert_eq!(2, map_array.value_length(1));
 
-//         for i in 0..sliced_array.len() {
-//             if bit_util::get_bit(&null_bits, sliced_array.offset() + i) {
-//                 assert!(sliced_array.is_valid(i));
-//             } else {
-//                 assert!(sliced_array.is_null(i));
-//             }
-//         }
+        let key_array = Arc::new(Int32Array::from(vec![3, 4, 5])) as ArrayRef;
+        let value_array = Arc::new(UInt32Array::from(vec![3, 4, 5])) as ArrayRef;
+        let struct_array =
+            StructArray::from(vec![(keys_field, key_array), (values_field, value_array)]);
+        assert_eq!(
+            &struct_array,
+            map_array
+                .value(0)
+                .as_any()
+                .downcast_ref::<StructArray>()
+                .unwrap()
+        );
+        assert_eq!(
+            &struct_array,
+            unsafe { map_array.value_unchecked(0) }
+                .as_any()
+                .downcast_ref::<StructArray>()
+                .unwrap()
+        );
+    }
 
-//         // Check offset and length for each non-null value.
-//         let sliced_list_array = sliced_array.as_any().downcast_ref::<MapArray>().unwrap();
-//         assert_eq!(2, sliced_list_array.value_offsets()[2]);
-//         assert_eq!(2, sliced_list_array.value_length(2));
-//         assert_eq!(4, sliced_list_array.value_offsets()[3]);
-//         assert_eq!(2, sliced_list_array.value_length(3));
-//         assert_eq!(6, sliced_list_array.value_offsets()[5]);
-//         assert_eq!(3, sliced_list_array.value_length(5));
-//     }
+    #[test]
+    fn test_map_array_slice() {
+        let map_array = create_from_buffers();
 
-//     #[test]
-//     #[should_panic(expected = "index out of bounds: the len is 10 but the index is 11")]
-//     fn test_map_array_index_out_of_bound() {
-//         // Construct a value array
-//         let value_data = ArrayData::builder(DataType::Int32)
-//             .len(10)
-//             .add_buffer(Buffer::from_slice_ref(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
-//             .build();
+        let sliced_array = map_array.slice(1, 2);
+        assert_eq!(2, sliced_array.len());
+        assert_eq!(1, sliced_array.offset());
 
-//         // Construct a buffer for value offsets, for the nested array:
-//         //  [[0, 1], null, null, [2, 3], [4, 5], null, [6, 7, 8], null, [9]]
-//         let value_offsets = Buffer::from_slice_ref(&[0i64, 2, 2, 2, 4, 6, 6, 9, 9, 10]);
-//         // 01011001 00000001
-//         let mut null_bits: [u8; 2] = [0; 2];
-//         bit_util::set_bit(&mut null_bits, 0);
-//         bit_util::set_bit(&mut null_bits, 3);
-//         bit_util::set_bit(&mut null_bits, 4);
-//         bit_util::set_bit(&mut null_bits, 6);
-//         bit_util::set_bit(&mut null_bits, 8);
+        // Check offset and length for each non-null value.
+        let sliced_map_array = sliced_array.as_any().downcast_ref::<MapArray>().unwrap();
+        assert_eq!(3, sliced_map_array.value_offsets()[0]);
+        assert_eq!(3, sliced_map_array.value_length(0));
+        assert_eq!(6, sliced_map_array.value_offsets()[1]);
+        assert_eq!(2, sliced_map_array.value_length(1));
+    }
 
-//         // Construct a map array from the above two
-//         let list_data_type =
-//             DataType::LargeList(Box::new(Field::new("item", DataType::Int32, false)));
-//         let list_data = ArrayData::builder(list_data_type)
-//             .len(9)
-//             .add_buffer(value_offsets)
-//             .add_child_data(value_data)
-//             .null_bit_buffer(Buffer::from(null_bits))
-//             .build();
-//         let list_array = MapArray::from(list_data);
-//         assert_eq!(9, list_array.len());
+    #[test]
+    #[should_panic(expected = "index out of bounds: the len is ")]
+    fn test_map_array_index_out_of_bound() {
+        let map_array = create_from_buffers();
 
-//         list_array.value(10);
-//     }
-
-//     #[test]
-//     #[should_panic(
-//         expected = "MapArray data should contain a single buffer only (value offsets)"
-//     )]
-//     fn test_map_array_invalid_buffer_len() {
-//         let value_data = ArrayData::builder(DataType::Int32)
-//             .len(8)
-//             .add_buffer(Buffer::from_slice_ref(&[0, 1, 2, 3, 4, 5, 6, 7]))
-//             .build();
-//         let list_data_type =
-//             DataType::List(Box::new(Field::new("item", DataType::Int32, false)));
-//         let list_data = ArrayData::builder(list_data_type)
-//             .len(3)
-//             .add_child_data(value_data)
-//             .build();
-//         MapArray::from(list_data);
-//     }
-
-//     #[test]
-//     #[should_panic(
-//         expected = "MapArray should contain a single child array (values array)"
-//     )]
-//     fn test_map_array_invalid_child_array_len() {
-//         let value_offsets = Buffer::from_slice_ref(&[0, 2, 5, 7]);
-//         let list_data_type =
-//             DataType::List(Box::new(Field::new("item", DataType::Int32, false)));
-//         let list_data = ArrayData::builder(list_data_type)
-//             .len(3)
-//             .add_buffer(value_offsets)
-//             .build();
-//         MapArray::from(list_data);
-//     }
-
-//     #[test]
-//     #[should_panic(expected = "offsets do not start at zero")]
-//     fn test_map_array_invalid_value_offset_start() {
-//         let value_data = ArrayData::builder(DataType::Int32)
-//             .len(8)
-//             .add_buffer(Buffer::from_slice_ref(&[0, 1, 2, 3, 4, 5, 6, 7]))
-//             .build();
-
-//         let value_offsets = Buffer::from_slice_ref(&[2, 2, 5, 7]);
-
-//         let list_data_type =
-//             DataType::List(Box::new(Field::new("item", DataType::Int32, false)));
-//         let list_data = ArrayData::builder(list_data_type)
-//             .len(3)
-//             .add_buffer(value_offsets)
-//             .add_child_data(value_data)
-//             .build();
-//         MapArray::from(list_data);
-//     }
-// }
+        map_array.value(map_array.len());
+    }
+}
