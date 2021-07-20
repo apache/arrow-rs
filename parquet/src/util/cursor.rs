6c82bdc20cdb9d78b8985df4466da2934b7650b5
Mykhailo Osypov <mykhailo.osypov@gmail.com>
2021-07-20 19:26:42 +0300

    Add len() to InMemoryWriteableCursor (#564)
    * Add len() to InMemoryWriteableCursor
    
    * Add is_empty to make clippy happy

--- parquet/src/util/cursor.rs
+++ parquet/src/util/cursor.rs
@@ -154,6 +154,18 @@ impl InMemoryWriteableCursor {
         let inner = self.buffer.lock().unwrap();
         inner.get_ref().to_vec()
     }
+
+    /// Returns a length of the underlying buffer
+    pub fn len(&self) -> usize {
+        let inner = self.buffer.lock().unwrap();
+        inner.get_ref().len()
+    }
+
+    /// Returns true if the underlying buffer contains no elements
+    pub fn is_empty(&self) -> bool {
+        let inner = self.buffer.lock().unwrap();
+        inner.get_ref().is_empty()
+    }
 }
 
 impl TryClone for InMemoryWriteableCursor {
