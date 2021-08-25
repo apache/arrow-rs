cc04f37c450e81d2dce7985134645477bb6fd975
Mathias Peters <mathias.peters@signavio.com>
2021-08-25 15:37:28 +0200

    Updated tests and improved implementation

--- arrow/src/array/transform/utils.rs
+++ arrow/src/array/transform/utils.rs
@@ -15,7 +15,14 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use crate::{array::OffsetSizeTrait, buffer::MutableBuffer, util::bit_util};
+use crate::{
+    array::OffsetSizeTrait,
+    buffer::MutableBuffer,
+    util::{
+        bit_chunk_iterator::BitChunks,
+        bit_util::{self, ceil},
+    },
+};
 
 /// extends the `buffer` to be able to hold `len` bits, setting all bits of the new size to zero.
 #[inline]
@@ -35,19 +42,36 @@ pub(super) fn set_bits(
     offset_read: usize,
     len: usize,
 ) -> usize {
-    // write to write_data until it is at a whole-byte offset
-    // create bit chunk iterator for data
-    // write whole bytes from iterator to write_data
+    let mut null_count = 0;
 
-    let mut count = 0;
-    (0..len).for_each(|i| {
-        if bit_util::get_bit(data, offset_read + i) {
-            bit_util::set_bit(write_data, offset_write + i);
-        } else {
-            count += 1;
-        }
+    let mut bits_to_align = offset_write % 8;
+    if bits_to_align > 0 {
+        bits_to_align = std::cmp::min(len, 8 - bits_to_align);
+    }
+    let bytes_to_copy = ceil(offset_write + bits_to_align, 8)
+        ..ceil(offset_write + len - bits_to_align, 8);
+
+    let chunks = BitChunks::new(data, offset_read + bits_to_align, len - bits_to_align);
+    let mut bytes: Vec<u8> = Vec::with_capacity(bytes_to_copy.len());
+    chunks.iter().for_each(|chunk| {
+        null_count += chunk.count_zeros();
+        bytes.extend_from_slice(&chunk.to_ne_bytes());
     });
-    count
+
+    let remainder_bits = &chunks.remainder_len();
+    (0..bits_to_align)
+        .chain(len - remainder_bits..len)
+        .for_each(|i| {
+            if bit_util::get_bit(data, offset_read + i) {
+                bit_util::set_bit(write_data, offset_write + i);
+            } else {
+                null_count += 1;
+            }
+        });
+    bytes_to_copy.zip(bytes.iter()).for_each(|(i, b)| {
+        write_data[i] = *b;
+    });
+    null_count as usize
 }
 
 pub(super) fn extend_offsets<T: OffsetSizeTrait>(
@@ -85,16 +109,22 @@ mod tests {
 
     #[test]
     fn test_set_bits_aligned() {
-        let mut destination: Vec<u8> = vec![0b01010101, 0b00000000];
-        let source: &[u8] = &[0b00110011];
+        let mut destination: Vec<u8> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let source: &[u8] = &[
+            0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111,
+            0b11100111, 0b11100111,
+        ];
 
         let destination_offset = 8;
         let source_offset = 0;
 
-        let len = 8;
+        let len = 64;
 
-        let expected_data: &[u8] = &[0b01010101, 0b00110011];
-        let expected_null_count = 4;
+        let expected_data: &[u8] = &[
+            0, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111,
+            0b11100111, 0b11100111, 0,
+        ];
+        let expected_null_count = 16;
         let result = set_bits(
             destination.as_mut_slice(),
             source,
@@ -108,17 +138,23 @@ mod tests {
     }
 
     #[test]
-    fn test_set_bits_unaligned() {
-        let mut destination: Vec<u8> = vec![0, 0];
-        let source: &[u8] = &[0b11111111];
+    fn test_set_bits_unaligned_unset() {
+        let mut destination: Vec<u8> = vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let source: &[u8] = &[
+            0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111,
+            0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111,
+        ];
 
         let destination_offset = 4;
-        let source_offset = 2;
+        let source_offset = 4;
 
-        let len = 6;
+        let len = 88;
 
-        let expected_data: &[u8] = &[0b11110000, 0b00000011];
-        let expected_null_count = 0;
+        let expected_data: &[u8] = &[
+            0b11100000, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111,
+            0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b11100111, 0b0000111,
+        ];
+        let expected_null_count = 22;
         let result = set_bits(
             destination.as_mut_slice(),
             source,
