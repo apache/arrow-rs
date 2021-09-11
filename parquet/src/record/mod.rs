5a935034c95485c99b763477b47ef1098fbdaf1c
Richard <huachao.huang@gmail.com>
2021-09-11 15:53:16 +0800

    Export `RowColumnIter` to fix doc (#763)
    * Export RowColumnIter to fix doc
    
    * Add documentation for RowColumnIter
    
    * Improve documentation for RowColumnIter

--- parquet/src/record/mod.rs
+++ parquet/src/record/mod.rs
@@ -23,6 +23,6 @@ mod record_writer;
 mod triplet;
 
 pub use self::{
-    api::{Field, List, ListAccessor, Map, MapAccessor, Row, RowAccessor},
+    api::{Field, List, ListAccessor, Map, MapAccessor, Row, RowAccessor, RowColumnIter},
     record_writer::RecordWriter,
 };
