5bd293d2cf3675f021261b69ace299b6b92f1c66
remzi <13716567376yh@gmail.com>
2022-02-04 10:58:51 +0800

    disallow cast ptr alignment
    Signed-off-by: remzi <13716567376yh@gmail.com>

--- parquet/src/lib.rs
+++ parquet/src/lib.rs
@@ -34,7 +34,6 @@
 #![allow(dead_code)]
 #![allow(non_camel_case_types)]
 #![allow(
-    clippy::cast_ptr_alignment,
     clippy::float_cmp,
     clippy::float_equality_without_abs,
     clippy::from_over_into,
