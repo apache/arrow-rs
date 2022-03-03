1a3819fd25086525786ae76d6eec0dcc340b9d20
yangjiang <yangjiang@ebay.com>
2022-03-03 12:31:26 +0800

    Add extract month in temporal.rs

--- arrow/src/compute/kernels/temporal.rs
+++ arrow/src/compute/kernels/temporal.rs
@@ -169,6 +169,23 @@ where
     Ok(b.finish())
 }
 
+/// Extracts the month of a given temporal array as an array of integers
+pub fn month<T>(array: &PrimitiveArray<T>) -> Result<Int32Array>
+where
+    T: ArrowTemporalType + ArrowNumericType,
+    i64: std::convert::From<T::Native>,
+{
+    let mut b = Int32Builder::new(array.len());
+    match array.data_type() {
+        &DataType::Date32 | &DataType::Date64 | &DataType::Timestamp(_, _) => {
+            extract_component_from_array!(array, b, month, value_as_datetime)
+        }
+        dt => return_compute_error_with!("year does not support", dt),
+    }
+
+    Ok(b.finish())
+}
+
 /// Extracts the minutes of a given temporal array as an array of integers
 pub fn minute<T>(array: &PrimitiveArray<T>) -> Result<Int32Array>
 where
@@ -299,6 +316,29 @@ mod tests {
         assert_eq!(2011, b.value(0));
         assert!(!b.is_valid(1));
         assert_eq!(2012, b.value(2));
+    }
+
+    #[test]
+    fn test_temporal_array_date64_month() {
+        //1514764800000 -> 2018-01-01
+        //1550636625000 -> 2019-02-20
+        let a: PrimitiveArray<Date64Type> =
+            vec![Some(1514764800000), None, Some(1550636625000)].into();
+
+        let b = month(&a).unwrap();
+        assert_eq!(01, b.value(0));
+        assert!(!b.is_valid(1));
+        assert_eq!(02, b.value(2));
+    }
+
+    #[test]
+    fn test_temporal_array_date32_month() {
+        let a: PrimitiveArray<Date32Type> = vec![Some(1), None, Some(31)].into();
+
+        let b = month(&a).unwrap();
+        assert_eq!(01, b.value(0));
+        assert!(!b.is_valid(1));
+        assert_eq!(02, b.value(2));
     }
 
     #[test]
