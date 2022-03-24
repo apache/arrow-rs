163decaaf207780ed4da1ac66220148262b97238
Liang-Chi Hsieh <viirya@gmail.com>
2022-03-24 10:57:22 -0700

    Add nullability and metadata

--- arrow/src/array/equal/utils.rs
+++ arrow/src/array/equal/utils.rs
@@ -72,11 +72,21 @@ pub(super) fn base_equal(lhs: &ArrayData, rhs: &ArrayData) -> bool {
                 (DataType::Struct(l_fields), DataType::Struct(r_fields))
                     if l_fields.len() == 2 && r_fields.len() == 2 =>
                 {
+                    let l_key_field = l_fields.get(0).unwrap();
+                    let r_key_field = r_fields.get(0).unwrap();
+                    let l_value_field = l_fields.get(1).unwrap();
+                    let r_value_field = r_fields.get(1).unwrap();
+
                     // We don't enforce the equality of field names
-                    l_fields.get(0).unwrap().data_type()
-                        == r_fields.get(0).unwrap().data_type()
-                        && l_fields.get(1).unwrap().data_type()
-                            == r_fields.get(1).unwrap().data_type()
+                    let data_type_equal = l_key_field.data_type()
+                        == r_key_field.data_type()
+                        && l_value_field.data_type() == r_value_field.data_type();
+                    let nullability_equal = l_key_field.is_nullable()
+                        == r_key_field.is_nullable()
+                        && l_value_field.is_nullable() == r_value_field.is_nullable();
+                    let metadata_equal = l_key_field.metadata() == r_key_field.metadata()
+                        && l_value_field.metadata() == r_value_field.metadata();
+                    data_type_equal && nullability_equal && metadata_equal
                 }
                 _ => panic!("Map type should have 2 fields Struct in its field"),
             };
