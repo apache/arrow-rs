225d86c8366141d0ef4d6460a550a7a837577d2b
Andrew Lamb <andrew@nerdnetworks.org>
2022-04-25 06:46:51 -0400

    fix clippy

--- arrow/src/array/array_list.rs
+++ arrow/src/array/array_list.rs
@@ -531,10 +531,10 @@ mod tests {
         // Construct a list array from the above two
         let list_data_type =
             DataType::List(Box::new(Field::new("item", DataType::Int32, false)));
-        let list_data = ArrayData::builder(list_data_type.clone())
+        let list_data = ArrayData::builder(list_data_type)
             .len(0)
-            .add_buffer(value_offsets.clone())
-            .add_child_data(value_data.clone())
+            .add_buffer(value_offsets)
+            .add_child_data(value_data)
             .build()
             .unwrap();
 
