dec3733bd34ccd49fd1f3c3879d4eea64d40a47a
Kamil Konior <cheappie.dev@gmail.com>
2022-05-01 13:19:01 +0200

    a try to improve method names

--- parquet/src/arrow/arrow_writer.rs
+++ parquet/src/arrow/arrow_writer.rs
@@ -113,27 +113,26 @@ impl<W: 'static + ParquetWriter> ArrowWriter<W> {
         }
 
         self.buffered_rows += batch.num_rows();
-
-        self.flush_completed()?;
+        self.flush_excess()?;
 
         Ok(())
     }
 
     /// Flushes buffered data until there are less than `max_row_group_size` rows buffered
-    fn flush_completed(&mut self) -> Result<()> {
+    fn flush_excess(&mut self) -> Result<()> {
         while self.buffered_rows >= self.max_row_group_size {
-            self.flush_row_group(self.max_row_group_size)?;
+            self.flush_batch_into_new_row_group(self.max_row_group_size)?;
         }
         Ok(())
     }
 
     /// Flushes `buffered_rows` from the buffer into a new row group
-    pub fn flush_buffered_rows(&mut self) -> Result<()> {
-        self.flush_row_group(self.buffered_rows)
+    pub fn flush_row_group(&mut self) -> Result<()> {
+        self.flush_batch_into_new_row_group(self.buffered_rows)
     }
 
     /// Flushes `num_rows` from the buffer into a new row group
-    fn flush_row_group(&mut self, num_rows: usize) -> Result<()> {
+    fn flush_batch_into_new_row_group(&mut self, num_rows: usize) -> Result<()> {
         if num_rows == 0 {
             return Ok(());
         }
@@ -197,8 +196,8 @@ impl<W: 'static + ParquetWriter> ArrowWriter<W> {
 
     /// Close and finalize the underlying Parquet writer
     pub fn close(&mut self) -> Result<parquet_format::FileMetaData> {
-        self.flush_completed()?;
-        self.flush_buffered_rows()?;
+        self.flush_excess()?;
+        self.flush_row_group()?;
         self.writer.close()
     }
 }
