a013778c48aa867fe6c1ac1e3ca1162087fddd3f
Jörn Horstmann <git@jhorstmann.net>
2022-05-26 12:15:22 +0200

    Optimize partition_validity

--- arrow/src/compute/kernels/sort.rs
+++ arrow/src/compute/kernels/sort.rs
@@ -24,6 +24,7 @@ use crate::datatypes::*;
 use crate::error::{ArrowError, Result};
 use std::cmp::Ordering;
 use TimeUnit::*;
+use crate::util::bit_util::get_bit_raw;
 
 /// Sort the `ArrayRef` using `SortOptions`.
 ///
@@ -151,8 +152,16 @@ fn partition_validity(array: &ArrayRef) -> (Vec<u32>, Vec<u32>) {
         // faster path
         0 => ((0..(array.len() as u32)).collect(), vec![]),
         _ => {
-            let indices = 0..(array.len() as u32);
-            indices.partition(|index| array.is_valid(*index as usize))
+            let validity = array.data().null_buffer().unwrap();
+            let offset = array.data().offset();
+            let mut vecs = [Vec::with_capacity(array.null_count()), Vec::with_capacity(array.len() - array.null_count())];
+            for i in 0..array.len() {
+                let bit = unsafe { get_bit_raw(validity.as_ptr(), offset + i ) };
+                vecs[bit as usize].push(i as u32);
+            }
+            let nulls = std::mem::take(&mut vecs[0]);
+            let valids = std::mem::take(&mut vecs[1]);
+            (valids, nulls)
         }
     }
 }
