c396dfb5035d22e57717b6dd365486b76eb611bc
Andrew Lamb <andrew@nerdnetworks.org>
2022-06-10 13:27:03 -0400

    Update versions and CHANGELOG for `16.0.0` (#1826)
    * Update versions to 16.0.0
    
    * Update changelog
    
    * Updates
    
    * updates
    
    * Update for latest
    
    * polish

--- dev/release/update_change_log.sh
+++ dev/release/update_change_log.sh
@@ -29,8 +29,8 @@
 
 set -e
 
-SINCE_TAG="14.0.0"
-FUTURE_RELEASE="15.0.0"
+SINCE_TAG="15.0.0"
+FUTURE_RELEASE="16.0.0"
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_TOP_DIR="$(cd "${SOURCE_DIR}/../../" && pwd)"
