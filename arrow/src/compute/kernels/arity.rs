4eaa2b1a764931a07ebc0cf64d5959d1b6c208df
Liang-Chi Hsieh <viirya@gmail.com>
2022-07-02 01:25:49 -0700

    Init

--- arrow/src/compute/kernels/arity.rs
+++ arrow/src/compute/kernels/arity.rs
@@ -17,9 +17,17 @@
 
 //! Defines kernels suitable to perform operations to primitive arrays.
 
-use crate::array::{Array, ArrayData, PrimitiveArray};
+use crate::array::{
+    Array, ArrayData, ArrayRef, DictionaryArray, Float16Array, Float32Array,
+    Float64Array, Int16Array, Int32Array, Int64Array, Int8Array, PrimitiveArray,
+    UInt16Array, UInt32Array, UInt64Array, UInt8Array,
+};
 use crate::buffer::Buffer;
-use crate::datatypes::ArrowPrimitiveType;
+use crate::datatypes::{
+    ArrowNumericType, ArrowPrimitiveType, DataType, Int16Type, Int32Type, Int64Type,
+    Int8Type, UInt16Type, UInt32Type, UInt64Type, UInt8Type,
+};
+use crate::error::{ArrowError, Result};
 
 #[inline]
 fn into_primitive_array_data<I: ArrowPrimitiveType, O: ArrowPrimitiveType>(
@@ -78,10 +86,215 @@ where
     PrimitiveArray::<O>::from(data)
 }
 
+macro_rules! unary_dict_op {
+    ($array: expr, $op: expr, $value_ty: ty) => {{
+        // Safety justification: Since the inputs are valid Arrow arrays, all values are
+        // valid indexes into the dictionary (which is verified during construction)
+
+        let array_iter = unsafe {
+            $array
+                .values()
+                .as_any()
+                .downcast_ref::<$value_ty>()
+                .unwrap()
+                .take_iter_unchecked($array.keys_iter())
+        };
+
+        let values = array_iter
+            .map(|v| {
+                if let Some(value) = v {
+                    Some($op(value))
+                } else {
+                    None
+                }
+            })
+            .collect();
+
+        Ok(values)
+    }};
+}
+
+/// Applies an unary function to a dictionary array with primitive value type.
+pub fn unary_dict<K, F, T>(array: &DictionaryArray<K>, op: F) -> Result<PrimitiveArray<T>>
+where
+    K: ArrowNumericType,
+    T: ArrowPrimitiveType,
+    F: Fn(T::Native) -> T::Native,
+{
+    unary_dict_op!(array, op, PrimitiveArray<T>)
+}
+
+macro_rules! unary_op {
+    ($array: expr, $op: expr, $op_type: ty, $value_ty: ty) => {{
+        match $array.data_type() {
+            DataType::Int8 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::Int16 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::Int32 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::Int64 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::UInt8 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::UInt16 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::UInt32 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::UInt64 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::Float16 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::Float32 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            DataType::Float64 => Ok(unary::<$value_ty, $op_type, $value_ty>(
+                $array
+                    .as_any()
+                    .downcast_ref::<PrimitiveArray<$value_ty>>()
+                    .unwrap(),
+                $op,
+            )),
+            t => Err(ArrowError::NotYetImplemented(format!(
+                "Cannot perform unary operation on array of type {}.",
+                t
+            ))),
+        }
+    }};
+}
+
+pub fn unary_dyn<F, T>(array: &dyn Array, op: F) -> Result<PrimitiveArray<T>>
+where
+    T: ArrowPrimitiveType,
+    F: Fn(T::Native) -> T::Native,
+{
+    match array.data_type() {
+        DataType::Dictionary(key_type, value_type) => match key_type.as_ref() {
+            DataType::Int8 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<Int8Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::Int16 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<Int16Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::Int32 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<Int32Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::Int64 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<Int64Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::UInt8 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<UInt8Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::UInt16 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<UInt16Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::UInt32 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<UInt32Type>>()
+                    .unwrap(),
+                op,
+            ),
+            DataType::UInt64 => unary_dict(
+                array
+                    .as_any()
+                    .downcast_ref::<DictionaryArray<UInt64Type>>()
+                    .unwrap(),
+                op,
+            ),
+            t => Err(ArrowError::NotYetImplemented(format!(
+                "Cannot perform unary operation on dictionary array of key type {}.",
+                t
+            ))),
+        },
+        _ => Ok(unary::<T, F, T>(
+            array.as_any().downcast_ref::<PrimitiveArray<T>>().unwrap(),
+            op,
+        )), // unary_op!(array, op, F, T),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::array::{as_primitive_array, Float64Array};
+    use crate::array::{
+        as_primitive_array, Float64Array, Int32Array, PrimitiveBuilder,
+        PrimitiveDictionaryBuilder,
+    };
+    use crate::datatypes::{Int32Type, Int8Type};
 
     #[test]
     fn test_unary_f64_slice() {
@@ -93,6 +306,26 @@ mod tests {
         assert_eq!(
             result,
             Float64Array::from(vec![None, Some(7.0), None, Some(7.0)])
+        )
+    }
+
+    #[test]
+    fn test_unary_dict() {
+        let key_builder = PrimitiveBuilder::<Int8Type>::new(3);
+        let value_builder = PrimitiveBuilder::<Int32Type>::new(2);
+        let mut builder = PrimitiveDictionaryBuilder::new(key_builder, value_builder);
+        builder.append(5).unwrap();
+        builder.append(6).unwrap();
+        builder.append(7).unwrap();
+        builder.append(8).unwrap();
+        builder.append_null().unwrap();
+        builder.append(9).unwrap();
+        let dictionary_array = builder.finish();
+
+        let result = unary_dict(&dictionary_array, |n| n + 1).unwrap();
+        assert_eq!(
+            result,
+            Int32Array::from(vec![Some(6), Some(7), Some(8), Some(9), None, Some(10)])
         )
     }
 }
