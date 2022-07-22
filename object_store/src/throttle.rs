01f3908d043bc8f54fa883e2926635e322b7674a
Andrew Lamb <andrew@nerdnetworks.org>
2022-07-22 13:42:15 -0400

    Only run object_store throttle tests on a mac

--- object_store/src/throttle.rs
+++ object_store/src/throttle.rs
@@ -314,6 +314,8 @@ mod tests {
     }
 
     #[tokio::test]
+    // macos github runner is so slow it can't complete within WAIT_TIME*2
+    #[cfg(target_os = "linux")]
     async fn get_test() {
         let inner = InMemory::new();
         let store = ThrottledStore::new(inner, ThrottleConfig::default());
@@ -341,6 +343,8 @@ mod tests {
     }
 
     #[tokio::test]
+    // macos github runner is so slow it can't complete within WAIT_TIME*2
+    #[cfg(target_os = "linux")]
     async fn list_test() {
         let inner = InMemory::new();
         let store = ThrottledStore::new(inner, ThrottleConfig::default());
@@ -366,6 +370,8 @@ mod tests {
     }
 
     #[tokio::test]
+    // macos github runner is so slow it can't complete within WAIT_TIME*2
+    #[cfg(target_os = "linux")]
     async fn list_with_delimiter_test() {
         let inner = InMemory::new();
         let store = ThrottledStore::new(inner, ThrottleConfig::default());
