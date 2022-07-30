ac6eb8e9858ae76eeca3248f28f9c88c5ba7dd94
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-07-30 20:51:01 +0100

    Add retry_timeout

--- object_store/src/client/retry.rs
+++ object_store/src/client/retry.rs
@@ -21,16 +21,12 @@ use crate::client::backoff::{Backoff, BackoffConfig};
 use futures::future::BoxFuture;
 use futures::FutureExt;
 use reqwest::{Response, Result};
+use std::time::{Duration, Instant};
 
 /// Contains the configuration for how to respond to server errors
 ///
 /// By default they will be retried up to some limit, using exponential
 /// backoff with jitter. See [`BackoffConfig`] for more information
-///
-/// Note: requests are retried without renewing credentials or regenerating
-/// the message payload, as such `max_retries * backoff.max_backoff` should
-/// be kept below 5 minutes to avoid errors due to expired credentials
-/// and/or request payloads
 ///
 #[derive(Debug, Clone)]
 pub struct RetryConfig {
@@ -41,6 +37,19 @@ pub struct RetryConfig {
     ///
     /// Set to 0 to disable retries
     pub max_retries: usize,
+
+    /// The maximum length of time from the initial request
+    /// after which no further retries will be attempted
+    ///
+    /// This not only bounds the length of time before a server
+    /// error will be surfaced to the application, but also bounds
+    /// the length of time a request's credentials must remain valid.
+    ///
+    /// As requests are retried without renewing credentials or
+    /// regenerating request payloads, this number should be kept
+    /// below 5 minutes to avoid errors due to expired credentials
+    /// and/or request payloads
+    pub retry_timeout: Duration,
 }
 
 impl Default for RetryConfig {
@@ -48,6 +57,7 @@ impl Default for RetryConfig {
         Self {
             backoff: Default::default(),
             max_retries: 10,
+            retry_timeout: Duration::from_secs(3 * 60),
         }
     }
 }
@@ -65,15 +75,18 @@ impl RetryExt for reqwest::RequestBuilder {
     fn send_retry(self, config: &RetryConfig) -> BoxFuture<'static, Result<Response>> {
         let mut backoff = Backoff::new(&config.backoff);
         let max_retries = config.max_retries;
+        let retry_timeout = config.retry_timeout;
 
         async move {
             let mut retries = 0;
+            let now = Instant::now();
 
             loop {
                 let s = self.try_clone().expect("request body must be cloneable");
                 match s.send().await {
                     Err(e)
                         if retries < max_retries
+                            && now.elapsed() < retry_timeout
                             && e.status()
                                 .map(|s| s.is_server_error())
                                 .unwrap_or(false) =>
