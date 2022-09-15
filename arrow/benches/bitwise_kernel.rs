6c33a20169c2a73dd9f4677bce69d744ce84dce9
liukun4515 <liukun@apache.org>
2022-09-15 22:44:20 +0800

    add bench for bitwise or xor not

--- arrow/benches/bitwise_kernel.rs
+++ arrow/benches/bitwise_kernel.rs
@@ -18,79 +18,104 @@
 #[macro_use]
 extern crate criterion;
 
+use arrow::compute::kernels::bitwise::{
+    bitwise_and, bitwise_and_scalar, bitwise_not, bitwise_or, bitwise_or_scalar,
+    bitwise_xor, bitwise_xor_scalar,
+};
+use arrow::datatypes::Int64Type;
 use criterion::{black_box, Criterion};
 use rand::RngCore;
-use arrow::compute::kernels::bitwise::{bitwise_and, bitwise_and_scalar};
-use arrow::datatypes::Int64Type;
 
 extern crate arrow;
 
 use arrow::util::bench_util::create_primitive_array;
 use arrow::util::test_util::seedable_rng;
 
-
 fn bitwise_array_benchmark(c: &mut Criterion) {
     let size = 64 * 1024_usize;
+    let left_without_null = create_primitive_array::<Int64Type>(size, 0 as f32);
+    let right_without_null = create_primitive_array::<Int64Type>(size, 0 as f32);
+    let left_with_null = create_primitive_array::<Int64Type>(size, 0.2_f32);
+    let right_with_null = create_primitive_array::<Int64Type>(size, 0.2_f32);
     // array and
-    let left = create_primitive_array::<Int64Type>(size, 0 as f32);
-    let right = create_primitive_array::<Int64Type>(size, 0 as f32);
     let mut group = c.benchmark_group("bench bitwise array: and");
     group.bench_function("bitwise array and, no nulls", |b| {
         b.iter(|| {
-            black_box(bitwise_and(&left, &right).unwrap())
+            black_box(bitwise_and(&left_without_null, &right_without_null).unwrap())
         })
     });
-    let left = create_primitive_array::<Int64Type>(size, 0.2 as f32);
-    let right = create_primitive_array::<Int64Type>(size, 0.2 as f32);
     group.bench_function("bitwise array and, 20% nulls", |b| {
-        b.iter(|| {
-            black_box(bitwise_and(&left, &right).unwrap())
-        })
+        b.iter(|| black_box(bitwise_and(&left_with_null, &right_with_null).unwrap()))
     });
     group.finish();
     // array or
     let mut group = c.benchmark_group("bench bitwise: or");
     group.bench_function("bitwise array or, no nulls", |b| {
-        b.iter(|| {
-            black_box(bitwise_and(&left, &right).unwrap())
-        })
+        b.iter(|| black_box(bitwise_or(&left_without_null, &right_without_null).unwrap()))
     });
-    let left = create_primitive_array::<Int64Type>(size, 0.2 as f32);
-    let right = create_primitive_array::<Int64Type>(size, 0.2 as f32);
     group.bench_function("bitwise array or, 20% nulls", |b| {
-        b.iter(|| {
-            black_box(bitwise_and(&left, &right).unwrap())
-        })
+        b.iter(|| black_box(bitwise_or(&left_with_null, &right_with_null).unwrap()))
     });
     group.finish();
     // xor
     let mut group = c.benchmark_group("bench bitwise: xor");
+    group.bench_function("bitwise array xor, no nulls", |b| {
+        b.iter(|| {
+            black_box(bitwise_xor(&left_without_null, &right_without_null).unwrap())
+        })
+    });
+    group.bench_function("bitwise array xor, 20% nulls", |b| {
+        b.iter(|| black_box(bitwise_xor(&left_with_null, &right_with_null).unwrap()))
+    });
+    group.finish();
     // not
     let mut group = c.benchmark_group("bench bitwise: not");
+    group.bench_function("bitwise array not, no nulls", |b| {
+        b.iter(|| black_box(bitwise_not(&left_without_null).unwrap()))
+    });
+    group.bench_function("bitwise array not, 20% nulls", |b| {
+        b.iter(|| black_box(bitwise_not(&left_with_null).unwrap()))
+    });
+    group.finish();
 }
 
 fn bitwise_array_scalar_benchmark(c: &mut Criterion) {
     let size = 64 * 1024_usize;
-    // array scalar and
-    let array = create_primitive_array::<Int64Type>(size, 0 as f32);
+    let array_without_null = create_primitive_array::<Int64Type>(size, 0 as f32);
+    let array_with_null = create_primitive_array::<Int64Type>(size, 0.2_f32);
     let scalar = seedable_rng().next_u64() as i64;
+    // array scalar and
     let mut group = c.benchmark_group("bench bitwise array scalar: and");
     group.bench_function("bitwise array scalar and, no nulls", |b| {
-        b.iter(|| {
-            black_box(bitwise_and_scalar(&array, scalar).unwrap())
-        })
+        b.iter(|| black_box(bitwise_and_scalar(&array_without_null, scalar).unwrap()))
     });
-    let array = create_primitive_array::<Int64Type>(size, 0.2 as f32);
     group.bench_function("bitwise array and, 20% nulls", |b| {
-        b.iter(|| {
-            black_box(bitwise_and_scalar(&array, scalar).unwrap())
-        })
+        b.iter(|| black_box(bitwise_and_scalar(&array_with_null, scalar).unwrap()))
     });
     group.finish();
     // array scalar or
-
+    let mut group = c.benchmark_group("bench bitwise array scalar: or");
+    group.bench_function("bitwise array scalar or, no nulls", |b| {
+        b.iter(|| black_box(bitwise_or_scalar(&array_without_null, scalar).unwrap()))
+    });
+    group.bench_function("bitwise array scalar or, 20% nulls", |b| {
+        b.iter(|| black_box(bitwise_or_scalar(&array_with_null, scalar).unwrap()))
+    });
+    group.finish();
     // array scalar xor
+    let mut group = c.benchmark_group("bench bitwise array scalar: xor");
+    group.bench_function("bitwise array scalar xor, no nulls", |b| {
+        b.iter(|| black_box(bitwise_xor_scalar(&array_without_null, scalar).unwrap()))
+    });
+    group.bench_function("bitwise array scalar xor, 20% nulls", |b| {
+        b.iter(|| black_box(bitwise_xor_scalar(&array_with_null, scalar).unwrap()))
+    });
+    group.finish();
 }
 
-criterion_group!(benches, bitwise_benchmark);
+criterion_group!(
+    benches,
+    bitwise_array_benchmark,
+    bitwise_array_scalar_benchmark
+);
 criterion_main!(benches);
