8307cb220ef91f24e5d39aa48d42efa0ba8f7de3
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-09-24 11:18:49 +0100

    Format

--- arrow-buffer/src/native.rs
+++ arrow-buffer/src/native.rs
@@ -44,14 +44,7 @@ mod private {
 ///
 /// Due to the above restrictions, this trait is sealed to prevent accidental misuse
 pub trait ArrowNativeType:
-    std::fmt::Debug
-    + Send
-    + Sync
-    + Copy
-    + PartialOrd
-    + Default
-    + private::Sealed
-    + 'static
+    std::fmt::Debug + Send + Sync + Copy + PartialOrd + Default + private::Sealed + 'static
 {
     /// Convert native type from usize.
     #[inline]
