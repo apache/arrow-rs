ba5b771159eb6f8faf81151b0a751362a0c8c9a5
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-10-14 16:00:07 +1300

    Filter DecimalArray as PrimitiveArray (#2637)

--- arrow/src/compute/kernels/filter.rs
+++ arrow/src/compute/kernels/filter.rs
@@ -338,6 +338,16 @@ fn filter_array(values: &dyn Array, predicate: &FilterPredicate) -> Result<Array
         // actually filter
         _ => downcast_primitive_array! {
             values => Ok(Arc::new(filter_primitive(values, predicate))),
+            DataType::Decimal128(p, s) => {
+                let values = values.as_any().downcast_ref::<Decimal128Array>().unwrap();
+                let filtered = filter_primitive(values, predicate);
+                Ok(Arc::new(filtered.with_precision_and_scale(*p, *s).unwrap()))
+            }
+            DataType::Decimal256(p, s) => {
+                let values = values.as_any().downcast_ref::<Decimal256Array>().unwrap();
+                let filtered = filter_primitive(values, predicate);
+                Ok(Arc::new(filtered.with_precision_and_scale(*p, *s).unwrap()))
+            }
             DataType::Boolean => {
                 let values = values.as_any().downcast_ref::<BooleanArray>().unwrap();
                 Ok(Arc::new(filter_boolean(values, predicate)))
