28f46f21c2b266edad853ef4bab3b9ff4f653696
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-10-17 20:27:43 +1300

    Format

--- arrow/src/row/mod.rs
+++ arrow/src/row/mod.rs
@@ -31,43 +31,43 @@
 //! # use arrow_array::cast::{as_primitive_array, as_string_array};
 //! # use arrow_array::types::Int32Type;
 //! # use arrow_schema::DataType;
-//! 
+//!
 //! let a1 = Arc::new(Int32Array::from_iter_values([-1, -1, 0, 3, 3])) as ArrayRef;
 //! let a2 = Arc::new(StringArray::from_iter_values(["a", "b", "c", "d", "d"])) as ArrayRef;
 //! let arrays = vec![a1, a2];
-//! 
+//!
 //! // Convert arrays to rows
 //! let mut converter = RowConverter::new(vec![
 //!     SortField::new(DataType::Int32),
 //!     SortField::new(DataType::Utf8),
 //! ]);
 //! let rows = converter.convert_columns(&arrays).unwrap();
-//! 
+//!
 //! // Compare rows
 //! for i in 0..4 {
 //!     assert!(rows.row(i) <= rows.row(i + 1));
 //! }
 //! assert_eq!(rows.row(3), rows.row(4));
-//! 
+//!
 //! // Convert rows back to arrays
 //! let converted = converter.convert_rows(&rows).unwrap();
 //! assert_eq!(arrays, converted);
-//! 
+//!
 //! // Compare rows from different arrays
 //! let a1 = Arc::new(Int32Array::from_iter_values([3, 4])) as ArrayRef;
 //! let a2 = Arc::new(StringArray::from_iter_values(["e", "f"])) as ArrayRef;
 //! let arrays = vec![a1, a2];
 //! let rows2 = converter.convert_columns(&arrays).unwrap();
-//! 
+//!
 //! assert!(rows.row(4) < rows2.row(0));
 //! assert!(rows.row(4) < rows2.row(1));
-//! 
+//!
 //! // Convert selection of rows back to arrays
 //! let selection = [rows.row(0), rows2.row(1), rows.row(2), rows2.row(0)];
 //! let converted = converter.convert_rows(selection).unwrap();
 //! let c1 = as_primitive_array::<Int32Type>(converted[0].as_ref());
 //! assert_eq!(c1.values(), &[-1, 4, 0, 3]);
-//! 
+//!
 //! let c2 = as_string_array(converted[1].as_ref());
 //! let c2_values: Vec<_> = c2.iter().flatten().collect();
 //! assert_eq!(&c2_values, &["a", "f", "c", "e"]);
