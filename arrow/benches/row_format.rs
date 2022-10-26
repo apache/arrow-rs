1e26940342e36e90541662d3f0d5ae8ef7d4aa29
Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>
2022-10-27 10:36:53 +1300

    Update arrow/benches/row_format.rs
    Co-authored-by: Andrew Lamb <andrew@nerdnetworks.org>

--- arrow/benches/row_format.rs
+++ arrow/benches/row_format.rs
@@ -45,6 +45,7 @@ fn do_bench(c: &mut Criterion, name: &str, cols: Vec<ArrayRef>) {
 
     let mut converter = RowConverter::new(fields);
     let rows = converter.convert_columns(&cols).unwrap();
+    // using a pre-prepared row converter should be faster than the first time
     c.bench_function(&format!("convert_columns_prepared {}", name), |b| {
         b.iter(|| black_box(converter.convert_columns(&cols).unwrap()));
     });
