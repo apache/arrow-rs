8e187ce1ea1f0b20e42ad41d25f935b9f9ad1ee8
Christian Salvati <81280761+src255@users.noreply.github.com>
2022-11-10 21:38:43 -0500

    Add alias for UTC
    Add a method to PrimitiveArray<T: ArrowTimestampType> for using UTC as
    the timezone.

--- arrow-array/src/array/primitive_array.rs
+++ arrow-array/src/array/primitive_array.rs
@@ -783,6 +783,11 @@ impl<T: ArrowTimestampType> PrimitiveArray<T> {
         self.with_timezone_opt(Some(timezone.into()))
     }
 
+    /// Construct a timestamp array with UTC
+    pub fn with_timezone_utc(&self) -> Self {
+        self.with_timezone("+00:00")
+    }
+
     /// Construct a timestamp array with an optional timezone
     pub fn with_timezone_opt(&self, timezone: Option<String>) -> Self {
         let array_data = unsafe {
@@ -1318,6 +1323,21 @@ mod tests {
             ]);
         assert_eq!(
             "PrimitiveArray<Timestamp(Millisecond, None)>\n[\n  2018-12-31T00:00:00,\n  2018-12-31T00:00:00,\n  1921-01-02T00:00:00,\n]",
+            format!("{:?}", arr)
+        );
+    }
+
+    #[test]
+    fn test_timestamp_utc_fmt_debug() {
+        let arr: PrimitiveArray<TimestampMillisecondType> =
+            TimestampMillisecondArray::from(vec![
+                1546214400000,
+                1546214400000,
+                -1546214400000,
+            ])
+            .with_timezone_utc();
+        assert_eq!(
+            "PrimitiveArray<Timestamp(Millisecond, Some(\"+00:00\"))>\n[\n  2018-12-31T00:00:00,\n  2018-12-31T00:00:00,\n  1921-01-02T00:00:00,\n]",
             format!("{:?}", arr)
         );
     }
