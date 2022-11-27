f985818012e6d0a56fca49487dbc13c516f4613c
Liang-Chi Hsieh <viirya@gmail.com>
2022-11-27 13:20:47 -0800

    Use SlicesIterator for ArrayData Equality (#3198)
    * Use SlicesIterator for ArrayData Equality
    
    * Use BitSliceIterator

--- arrow-data/src/equal/fixed_binary.rs
+++ arrow-data/src/equal/fixed_binary.rs
@@ -15,7 +15,10 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use crate::data::{contains_nulls, ArrayData};
+use crate::bit_iterator::BitSliceIterator;
+use crate::contains_nulls;
+use crate::data::ArrayData;
+use crate::equal::primitive::NULL_SLICES_SELECTIVITY_THRESHOLD;
 use arrow_buffer::bit_util::get_bit;
 use arrow_schema::DataType;
 
@@ -47,26 +50,55 @@ pub(super) fn fixed_binary_equal(
             size * len,
         )
     } else {
-        // get a ref of the null buffer bytes, to use in testing for nullness
-        let lhs_null_bytes = lhs.null_buffer().as_ref().unwrap().as_slice();
-        let rhs_null_bytes = rhs.null_buffer().as_ref().unwrap().as_slice();
-        // with nulls, we need to compare item by item whenever it is not null
-        (0..len).all(|i| {
-            let lhs_pos = lhs_start + i;
-            let rhs_pos = rhs_start + i;
+        let selectivity_frac = lhs.null_count() as f64 / lhs.len() as f64;
 
-            let lhs_is_null = !get_bit(lhs_null_bytes, lhs_pos + lhs.offset());
-            let rhs_is_null = !get_bit(rhs_null_bytes, rhs_pos + rhs.offset());
+        if selectivity_frac >= NULL_SLICES_SELECTIVITY_THRESHOLD {
+            // get a ref of the null buffer bytes, to use in testing for nullness
+            let lhs_null_bytes = lhs.null_buffer().as_ref().unwrap().as_slice();
+            let rhs_null_bytes = rhs.null_buffer().as_ref().unwrap().as_slice();
+            // with nulls, we need to compare item by item whenever it is not null
+            (0..len).all(|i| {
+                let lhs_pos = lhs_start + i;
+                let rhs_pos = rhs_start + i;
 
-            lhs_is_null
-                || (lhs_is_null == rhs_is_null)
-                    && equal_len(
-                        lhs_values,
-                        rhs_values,
-                        lhs_pos * size,
-                        rhs_pos * size,
-                        size, // 1 * size since we are comparing a single entry
-                    )
-        })
+                let lhs_is_null = !get_bit(lhs_null_bytes, lhs_pos + lhs.offset());
+                let rhs_is_null = !get_bit(rhs_null_bytes, rhs_pos + rhs.offset());
+
+                lhs_is_null
+                    || (lhs_is_null == rhs_is_null)
+                        && equal_len(
+                            lhs_values,
+                            rhs_values,
+                            lhs_pos * size,
+                            rhs_pos * size,
+                            size, // 1 * size since we are comparing a single entry
+                        )
+            })
+        } else {
+            let lhs_slices_iter = BitSliceIterator::new(
+                lhs.null_buffer().as_ref().unwrap(),
+                lhs_start + lhs.offset(),
+                len,
+            );
+            let rhs_slices_iter = BitSliceIterator::new(
+                rhs.null_buffer().as_ref().unwrap(),
+                rhs_start + rhs.offset(),
+                len,
+            );
+
+            lhs_slices_iter.zip(rhs_slices_iter).all(
+                |((l_start, l_end), (r_start, r_end))| {
+                    l_start == r_start
+                        && l_end == r_end
+                        && equal_len(
+                            lhs_values,
+                            rhs_values,
+                            (lhs_start + l_start) * size,
+                            (rhs_start + r_start) * size,
+                            (l_end - l_start) * size,
+                        )
+                },
+            )
+        }
     }
 }
