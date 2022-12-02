206aee3f5a9e759458f24ea50de296b998187c6d
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-12-02 11:53:40 +0000

    Format

--- parquet/src/file/properties.rs
+++ parquet/src/file/properties.rs
@@ -81,7 +81,8 @@ const DEFAULT_DICTIONARY_PAGE_SIZE_LIMIT: usize = DEFAULT_PAGE_SIZE;
 const DEFAULT_STATISTICS_ENABLED: EnabledStatistics = EnabledStatistics::Page;
 const DEFAULT_MAX_STATISTICS_SIZE: usize = 4096;
 const DEFAULT_MAX_ROW_GROUP_SIZE: usize = 1024 * 1024;
-const DEFAULT_CREATED_BY: &str = concat!("parquet-rs version ", env!("CARGO_PKG_VERSION"));
+const DEFAULT_CREATED_BY: &str =
+    concat!("parquet-rs version ", env!("CARGO_PKG_VERSION"));
 /// default value for the false positive probability used in a bloom filter.
 pub const DEFAULT_BLOOM_FILTER_FPP: f64 = 0.05;
 /// default value for the expected number of distinct values used in a bloom filter.
