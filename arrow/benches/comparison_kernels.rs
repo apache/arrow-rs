fb6c4d3cabb1242b71e099bcd3ee7e3508f61ace
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2022-12-09 18:03:58 +0000

    Fix benchmark name

--- arrow/benches/comparison_kernels.rs
+++ arrow/benches/comparison_kernels.rs
@@ -326,7 +326,7 @@ fn add_benchmark(c: &mut Criterion) {
         b.iter(|| eq_dyn_utf8_scalar(&dict_arr_a, "test"))
     });
 
-    c.bench_function("eq_dyn_utf8_scalar scalar dictionary[10] string[4])", |b| {
+    c.bench_function("gt_eq_dyn_utf8_scalar scalar dictionary[10] string[4])", |b| {
         b.iter(|| gt_eq_dyn_utf8_scalar(&dict_arr_a, "test"))
     });
 
