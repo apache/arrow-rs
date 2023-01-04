adf70d1b371d0a6364f9139aa1258858841eb99d
Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>
2023-01-04 18:21:04 +0000

    Re-export nullif kernel (#3451)

--- arrow/src/compute/kernels/mod.rs
+++ arrow/src/compute/kernels/mod.rs
@@ -23,7 +23,7 @@ pub use arrow_arith::{aggregate, arithmetic, arity, bitwise, boolean, temporal};
 pub use arrow_cast::cast;
 pub use arrow_cast::parse as cast_utils;
 pub use arrow_ord::{partition, sort};
-pub use arrow_select::{concat, filter, interleave, take, window, zip};
+pub use arrow_select::{concat, filter, interleave, nullif, take, window, zip};
 pub use arrow_string::{concat_elements, length, regexp, substring};
 
 /// Comparison kernels for `Array`s.
