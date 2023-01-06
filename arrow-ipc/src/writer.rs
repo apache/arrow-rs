b4d57059e4b375a7b2128e430865390a5aa81510
Liang-Chi Hsieh <viirya@gmail.com>
2023-01-06 09:47:56 -0800

    Add a test for stream writer for writing sliced array (#3472)

--- arrow-ipc/src/writer.rs
+++ arrow-ipc/src/writer.rs
@@ -1890,4 +1890,40 @@ mod tests {
         assert!(serialize(&record_batch).len() > serialize(&record_batch_slice).len());
         assert_eq!(record_batch_slice, deserialized_batch);
     }
+
+    #[test]
+    fn test_stream_writer_writes_array_slice() {
+        let array = UInt32Array::from(vec![Some(1), Some(2), Some(3)]);
+        assert_eq!(
+            vec![Some(1), Some(2), Some(3)],
+            array.iter().collect::<Vec<_>>()
+        );
+
+        let sliced = array.slice(1, 2);
+        let read_sliced: &UInt32Array = as_primitive_array(&sliced);
+        assert_eq!(
+            vec![Some(2), Some(3)],
+            read_sliced.iter().collect::<Vec<_>>()
+        );
+
+        let batch = RecordBatch::try_new(
+            Arc::new(Schema::new(vec![Field::new("a", DataType::UInt32, true)])),
+            vec![sliced],
+        )
+        .expect("new batch");
+
+        let mut writer =
+            StreamWriter::try_new(vec![], &batch.schema()).expect("new writer");
+        writer.write(&batch).expect("write");
+        let outbuf = writer.into_inner().expect("inner");
+
+        let mut reader = StreamReader::try_new(&outbuf[..], None).expect("new reader");
+        let read_batch = reader.next().unwrap().expect("read batch");
+
+        let read_array: &UInt32Array = as_primitive_array(read_batch.column(0));
+        assert_eq!(
+            vec![Some(2), Some(3)],
+            read_array.iter().collect::<Vec<_>>()
+        );
+    }
 }
