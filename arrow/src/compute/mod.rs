c653769ce43712772af6a1621678f012a4862d57
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2023-01-12 11:18:11 +0100

    Additional nullif re-export

--- arrow/src/compute/mod.rs
+++ arrow/src/compute/mod.rs
@@ -29,6 +29,7 @@ pub use self::kernels::concat::*;
 pub use self::kernels::filter::*;
 pub use self::kernels::interleave::*;
 pub use self::kernels::limit::*;
+pub use self::kernels::nullif::*;
 pub use self::kernels::partition::*;
 pub use self::kernels::regexp::*;
 pub use self::kernels::sort::*;
