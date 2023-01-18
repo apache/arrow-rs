897d23cf2e9be30e07b6a50c3ad37212dbec0e43
sachin agarwal <asachin591@gmail.com>
2023-01-18 17:20:58 +0530

    Update parquet/src/arrow/async_reader/mod.rs
    Co-authored-by: Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>

--- parquet/src/arrow/async_reader/mod.rs
+++ parquet/src/arrow/async_reader/mod.rs
@@ -199,9 +199,8 @@ impl<T: AsyncRead + AsyncSeek + Unpin + Send> AsyncFileReader for T {
             self.seek(SeekFrom::End(-FOOTER_SIZE_I64 - metadata_len as i64))
                 .await?;
 
-            let mut buf_meta = vec![0_u8; metadata_len];
-            self.read_exact(buf_meta.as_mut_slice()).await?;
-            buf_meta.extend(buf);
+            let mut buf_meta = Vec::with_capacity(metadata_len);
+            self.take(metadata_len as _).read_to_end(&mut buf).await?;
 
             Ok(Arc::new(decode_metadata(&buf_meta)?))
         }
