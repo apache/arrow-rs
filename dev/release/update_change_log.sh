c434256e866bcb1c3462128c58d473c9301c2c72
Ian Alexander Joiner <14581281+iajoiner@users.noreply.github.com>
2023-03-09 17:29:04 -0500

    update change log script

--- dev/release/update_change_log.sh
+++ dev/release/update_change_log.sh
@@ -29,8 +29,8 @@
 
 set -e
 
-SINCE_TAG="33.0.0"
-FUTURE_RELEASE="34.0.0"
+SINCE_TAG="34.0.0"
+FUTURE_RELEASE="35.0.0"
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_TOP_DIR="$(cd "${SOURCE_DIR}/../../" && pwd)"
