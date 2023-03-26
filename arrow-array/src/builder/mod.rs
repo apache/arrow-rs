666d31628cc2de2fb608b16e03630bbd8a560e22
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2023-03-26 14:46:30 +0100

    Improve array builder documentation (#3949)

--- arrow-array/src/builder/mod.rs
+++ arrow-array/src/builder/mod.rs
@@ -15,7 +15,140 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//! Defines builders for the various array types
+//! Defines builders that can be used to safely build arrays
+//!
+//! # Basic Usage
+//!
+//! Builders can be used to build simple, non-nested arrays
+//!
+//! ```
+//! # use arrow_array::builder::Int32Builder;
+//! # use arrow_array::PrimitiveArray;
+//! let mut a = Int32Builder::new();
+//! a.append_value(1);
+//! a.append_null();
+//! a.append_value(2);
+//! let a = a.finish();
+//!
+//! assert_eq!(a, PrimitiveArray::from(vec![Some(1), None, Some(2)]));
+//! ```
+//!
+//! ```
+//! # use arrow_array::builder::StringBuilder;
+//! # use arrow_array::{Array, StringArray};
+//! let mut a = StringBuilder::new();
+//! a.append_value("foo");
+//! a.append_value("bar");
+//! a.append_null();
+//! let a = a.finish();
+//!
+//! assert_eq!(a, StringArray::from_iter([Some("foo"), Some("bar"), None]));
+//! ```
+//!
+//! # Nested Usage
+//!
+//! Builders can also be used to build more complex nested arrays, such as lists
+//!
+//! ```
+//! # use arrow_array::builder::{Int32Builder, ListBuilder};
+//! # use arrow_array::ListArray;
+//! # use arrow_array::types::Int32Type;
+//! let mut a = ListBuilder::new(Int32Builder::new());
+//! // [1, 2]
+//! a.values().append_value(1);
+//! a.values().append_value(2);
+//! a.append(true);
+//! // null
+//! a.append(false);
+//! // []
+//! a.append(true);
+//! // [3, null]
+//! a.values().append_value(3);
+//! a.values().append_null();
+//! a.append(true);
+//!
+//! // [[1, 2], null, [], [3, null]]
+//! let a = a.finish();
+//!
+//! assert_eq!(a, ListArray::from_iter_primitive::<Int32Type, _, _>([
+//!     Some(vec![Some(1), Some(2)]),
+//!     None,
+//!     Some(vec![]),
+//!     Some(vec![Some(3), None])]
+//! ))
+//! ```
+//!
+//! # Row Conversion
+//!
+//! It is common to have a statically defined row representation, and to want to convert
+//! this to an arrow representation. An example of this can be seen below
+//!
+//! ```
+//! use std::any::Any;
+//! use arrow_array::builder::{ArrayBuilder, Int32Builder, ListBuilder, StringBuilder};
+//! use arrow_array::{ArrayRef, RecordBatch, StructArray};
+//! use arrow_schema::{DataType, Field};
+//! use std::sync::Arc;
+//! /// A representation of a row
+//! struct Row {
+//!     i32: i32,
+//!     optional_i32: Option<i32>,
+//!     string: Option<String>,
+//!     i32_list: Option<Vec<Option<i32>>>,
+//! }
+//!
+//! /// Converts `Vec<Row>` into `StructArray`
+//! #[derive(Debug, Default)]
+//! struct RowBuilder {
+//!     i32: Int32Builder,
+//!     string: StringBuilder,
+//!     i32_list: ListBuilder<Int32Builder>,
+//! }
+//!
+//! impl<'a> Extend<&'a Row> for RowBuilder {
+//!     fn extend<T: IntoIterator<Item = &'a Row>>(&mut self, iter: T) {
+//!         for row in iter {
+//!             self.i32.append_value(row.i32);
+//!             self.string.append_option(row.string.as_ref());
+//!             match &row.i32_list {
+//!                 Some(list) => {
+//!                     list.iter().for_each(|v| self.i32_list.values().append_option(*v));
+//!                     self.i32_list.append(true);
+//!                 }
+//!                 None => self.i32_list.append(false),
+//!             }
+//!         }
+//!     }
+//! }
+//!
+//! impl RowBuilder {
+//!     /// Note: returns StructArray to allow nesting within another array if desired
+//!     fn finish(&mut self) -> StructArray {
+//!         let i32 = Arc::new(self.i32.finish()) as ArrayRef;
+//!         let i32_field = Field::new("i32", DataType::Int32, false);
+//!
+//!         let string = Arc::new(self.string.finish()) as ArrayRef;
+//!         let string_field = Field::new("i32", DataType::Utf8, false);
+//!
+//!         let i32_list = Arc::new(self.i32_list.finish()) as ArrayRef;
+//!         let value_field = Box::new(Field::new("item", DataType::Int32, true));
+//!         let i32_list_field = Field::new("i32_list", DataType::List(value_field), true);
+//!
+//!         StructArray::from(vec![
+//!             (i32_field, i32),
+//!             (string_field, string),
+//!             (i32_list_field, i32_list),
+//!         ])
+//!     }
+//! }
+//!
+//! /// Converts a slice of [`Row`] to a [`RecordBatch`]
+//! fn rows_to_batch(rows: &[Row]) -> RecordBatch {
+//!     let mut builder = RowBuilder::default();
+//!     builder.extend(rows);
+//!     RecordBatch::from(&builder.finish())
+//! }
+//! ```
 
 mod boolean_buffer_builder;
 pub use boolean_buffer_builder::*;
