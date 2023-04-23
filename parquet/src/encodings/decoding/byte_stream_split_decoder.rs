a4f98a3d3be86cfde9f3db15e8f54d0757de89da
Simon Vandel Sillesen <simon.vandel@gmail.com>
2023-04-23 10:23:47 +0000

    decoding works

--- parquet/src/encodings/decoding/byte_stream_split_decoder.rs
+++ parquet/src/encodings/decoding/byte_stream_split_decoder.rs
@@ -11,7 +11,7 @@ pub struct ByteStreamSplitDecoder<T: DataType> {
     _phantom: PhantomData<T>,
     encoded_bytes: ByteBufferPtr,
     total_num_values: usize,
-    cur_byte_idx: usize,
+    values_decoded: usize,
 }
 
 impl<T: DataType> ByteStreamSplitDecoder<T> {
@@ -20,7 +20,7 @@ impl<T: DataType> ByteStreamSplitDecoder<T> {
             _phantom: PhantomData,
             encoded_bytes: ByteBufferPtr::new(vec![]),
             total_num_values: 0,
-            cur_byte_idx: 0,
+            values_decoded: 0,
         }
     }
 }
@@ -29,7 +29,7 @@ impl<T: DataType> Decoder<T> for ByteStreamSplitDecoder<T> {
     fn set_data(&mut self, data: ByteBufferPtr, num_values: usize) -> Result<()> {
         self.encoded_bytes = data;
         self.total_num_values = num_values;
-        self.cur_byte_idx = 0;
+        self.values_decoded = 0;
 
         println!();
         Ok(())
@@ -37,36 +37,49 @@ impl<T: DataType> Decoder<T> for ByteStreamSplitDecoder<T> {
 
     fn get(&mut self, buffer: &mut [<T as DataType>::T]) -> Result<usize> {
         let total_remaining_values = self.values_left();
-        let num_values_to_decode = buffer.len().min(total_remaining_values);
+        let num_values = buffer.len().min(total_remaining_values);
         dbg!(
             self.total_num_values,
             total_remaining_values,
-            self.cur_byte_idx,
-            num_values_to_decode,
+            self.values_decoded,
+            num_values,
         );
-
-        let num_streams = T::get_type_size();
 
         // TODO explain safety
         let raw_out_bytes = unsafe { <T as DataType>::T::slice_as_bytes_mut(buffer) };
 
-        let byte_stride = num_values_to_decode;
-
-        let mut start = self.cur_byte_idx;
-
         // For each value, compute a chunk that is the indexes into self.encoded_bytes to fetch and combine into the output byte
 
-        (0..12).cycle().chun(T::get_type_size())
+        // TODO it might be better to go through one byte stream at a time for memory locality
 
-        for value_idx in 0..num_values_to_decode {
-            usize::
-            // go through each byte at a time of the value
-            for b_idx in 0..T::get_type_size() {
-                let encoded_byte_idx = 
-                raw_out_bytes[value_idx * T::get_type_size() + b_idx] =
-                    self.encoded_bytes[start + value_idx + (b_idx * T::get_type_size())];
+        let num_values = num_values;
+        let num_streams = T::get_type_size();
+        let byte_stream_length = self.encoded_bytes.len() / num_streams;
+        let values_decoded = self.values_decoded;
+
+        // go through each value to decode
+        for out_value_idx in 0..num_values {
+            // go through each byte stream of that value
+            for byte_stream_idx in 0..num_streams {
+                let idx_in_encoded_data = (byte_stream_idx * byte_stream_length)
+                    + (values_decoded + out_value_idx);
+
+                raw_out_bytes[(out_value_idx * num_streams) + byte_stream_idx] =
+                    self.encoded_bytes[idx_in_encoded_data];
             }
         }
+
+        // let encoded_indices_iter = (0..num_values).cartesian_product(0..num_streams).map(
+        //     |(value_idx, byte_idx)| {
+        //         (start
+        //             + value_idx
+        //             + (byte_idx * (num_streams - 1)) % self.encoded_bytes.len())
+        //     },
+        // );
+        // for (out_byte_idx, encoded_idx) in (0..num_bytes).zip(encoded_indices_iter) {
+        //     println!("{out_byte_idx} {encoded_idx}");
+        //     raw_out_bytes[out_byte_idx] = self.encoded_bytes[encoded_idx];
+        // }
 
         // for out_index in (0..raw_out_bytes.len()).step_by(T::get_type_size()) {
         //     for i in 0..T::get_type_size() {
@@ -90,13 +103,13 @@ impl<T: DataType> Decoder<T> for ByteStreamSplitDecoder<T> {
         // TODO I think there's an inherent flaw in the design of Decoding, where the
         // Or maybe we can actually do it by remember the byte we are at in self.buffer
 
-        self.cur_byte_idx += num_values_to_decode;
+        self.values_decoded += num_values;
 
-        Ok(num_values_to_decode)
+        Ok(num_values)
     }
 
     fn values_left(&self) -> usize {
-        self.total_num_values - self.cur_byte_idx
+        self.total_num_values - self.values_decoded
     }
 
     fn encoding(&self) -> Encoding {
