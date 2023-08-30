a73ef7cc3db99cb973c56538c9012151943dea4c
Raphael Taylor-Davies <r.taylordavies@googlemail.com>
2023-08-30 17:39:49 +0100

    Stricter view verification

--- arrow-data/src/view.rs
+++ arrow-data/src/view.rs
@@ -90,7 +90,12 @@ where
     for (idx, v) in views.iter().enumerate() {
         let len = *v as u32;
         if len <= 12 {
-            f(idx, &v.to_le_bytes()[4..4 + len as usize])?
+            if len < 12 && (v >> (32 + len * 8)) != 0 {
+                return Err(ArrowError::InvalidArgumentError(format!(
+                    "View at index {idx} contained non-zero padding for string of length {len}",
+                )));
+            }
+            f(idx, &v.to_le_bytes()[4..4 + len as usize])?;
         } else {
             let view = View::from(*v);
             let data = buffers.get(view.buffer_index as usize).ok_or_else(|| {
@@ -121,4 +126,67 @@ where
         }
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_validate() {
+        let buffers = vec![
+            Buffer::from(b"helloworldbongo\xFFfoo"),
+            Buffer::from(b"bar"),
+        ];
+
+        let test = |length: u32, prefix: u32, buffer_index: u32, offset: u32| {
+            let view = View {
+                length,
+                prefix,
+                buffer_index,
+                offset,
+            };
+            validate_string_view(&[view.into()], &buffers)
+        };
+
+        let err = test(14, 0, 2, 0).unwrap_err().to_string();
+        assert_eq!(err, "Invalid argument error: Invalid buffer index at 0: got index 2 but only has 2 buffers");
+
+        let err = test(14, 0, 1, 3).unwrap_err().to_string();
+        assert_eq!(err, "Invalid argument error: Invalid buffer slice at 0: got 3..17 but buffer 1 has length 3");
+
+        let err = test(14, 0, 1, 3).unwrap_err().to_string();
+        assert_eq!(err, "Invalid argument error: Invalid buffer slice at 0: got 3..17 but buffer 1 has length 3");
+
+        let err = test(15, 0, 0, 0).unwrap_err().to_string();
+        assert_eq!(
+            err,
+            "Invalid argument error: Mismatch between embedded prefix and data"
+        );
+
+        let prefix = u32::from_le_bytes(*b"hell");
+        test(15, prefix, 0, 0).unwrap();
+
+        let prefix = u32::from_le_bytes(*b"ello");
+        let err = test(15, prefix, 0, 1).unwrap_err().to_string();
+        assert_eq!(
+            err,
+            "Invalid argument error: Encountered non-UTF-8 data at index 0: invalid utf-8 sequence of 1 bytes from index 14"
+        );
+
+        let encoded = u128::from_le_bytes(*b"\x05\0\0\0hello\0\0\0\0\0\0\0");
+        validate_string_view(&[encoded], &[]).unwrap();
+
+        let encoded = u128::from_le_bytes(*b"\x05\0\0\0hello\0\0\0s\0\0\0");
+        let err = validate_string_view(&[encoded], &[])
+            .unwrap_err()
+            .to_string();
+        assert_eq!(err, "Invalid argument error: View at index 0 contained non-zero padding for string of length 5");
+
+        let encoded = u128::from_le_bytes(*b"\x05\0\0\0he\xFFlo\0\0\0\0\0\0\0");
+        let err = validate_string_view(&[encoded], &[])
+            .unwrap_err()
+            .to_string();
+        assert_eq!(err, "Invalid argument error: Encountered non-UTF-8 data at index 0: invalid utf-8 sequence of 1 bytes from index 2");
+    }
 }
