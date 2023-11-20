b64b0530d8877e03b80804f9397a0e0b200c4169
fan <yfan3763@gmail.com>
2023-11-20 20:03:17 +0800

    follow reviews
    Signed-off-by: fan <yfan3763@gmail.com>

--- arrow-json/src/reader/string_array.rs
+++ arrow-json/src/reader/string_array.rs
@@ -97,21 +97,26 @@ impl<O: OffsetSizeTrait> ArrayDecoder for StringArrayDecoder<O> {
                 TapeElement::Number(idx) if coerce_primitive => {
                     builder.append_value(tape.get_string(idx));
                 }
-                TapeElement::I64(high) if coerce_primitive => {
-                    match tape.get(p + 1) {
-                        TapeElement::I32(low) => {
-                            let val = (high as i64) << 32 | (low as u32) as i64;
-                            builder.append_value(val.to_string());
-                        }
-                        _ => unreachable!(),
+                TapeElement::I64(high) if coerce_primitive => match tape.get(p + 1) {
+                    TapeElement::I32(low) => {
+                        let val = (high as i64) << 32 | (low as u32) as i64;
+                        builder.append_value(val.to_string());
                     }
-                }
+                    _ => unreachable!(),
+                },
                 TapeElement::I32(n) if coerce_primitive => {
                     builder.append_value(n.to_string());
                 }
-                TapeElement::F32(n) | TapeElement::F64(n) if coerce_primitive => {
+                TapeElement::F32(n) if coerce_primitive => {
                     builder.append_value(n.to_string());
                 }
+                TapeElement::F64(high) if coerce_primitive => match tape.get(p + 1) {
+                    TapeElement::F32(low) => {
+                        let val = f64::from_bits((high as u64) << 32 | low as u64);
+                        builder.append_value(val.to_string());
+                    }
+                    _ => unreachable!(),
+                },
                 _ => unreachable!(),
             }
         }
