d5795c2e4a7cc796c89750014bc605392dac412c
my-vegetable-has-exploded <wy1109468038@gmail.com>
2023-12-18 23:20:54 +0800

    fix clippy

--- arrow-cast/src/cast.rs
+++ arrow-cast/src/cast.rs
@@ -9518,7 +9518,7 @@ mod tests {
             ]
             .into(),
         );
-        assert_eq!(can_cast_types(&struct_type, &to_type), false);
+		assert!(!can_cast_types(&struct_type, &to_type));
         let result = cast(&struct_array, &to_type);
         assert_eq!(
             "Cast error: Casting from Boolean to Date32 not supported",
