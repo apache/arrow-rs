c6f8bb5b913216695aa6843dfc8d7b62505861a3
Justin Abrahms <justin@abrah.ms>
2023-12-23 05:26:51 -0800

    Document default value of InstanceCredentialProvider (#5188)
    * Document default value of InstanceCredentialProvider
    
    * Move docs to credentials field rather than crate-private method
    
    * Review feedback
    
    * Tweak doctest
    
    ---------
    
    Co-authored-by: Raphael Taylor-Davies <r.taylordavies@googlemail.com>

--- object_store/src/gcp/builder.rs
+++ object_store/src/gcp/builder.rs
@@ -78,18 +78,16 @@ impl From<Error> for crate::Error {
     }
 }
 
-/// Configure a connection to Google Cloud Storage using the specified
-/// credentials.
+/// Configure a connection to Google Cloud Storage.
+///
+/// If no credentials are explicitly provided, they will be sourced
+/// from the environment as documented [here](https://cloud.google.com/docs/authentication/application-default-credentials).
 ///
 /// # Example
 /// ```
 /// # let BUCKET_NAME = "foo";
-/// # let SERVICE_ACCOUNT_PATH = "/tmp/foo.json";
 /// # use object_store::gcp::GoogleCloudStorageBuilder;
-/// let gcs = GoogleCloudStorageBuilder::new()
-///  .with_service_account_path(SERVICE_ACCOUNT_PATH)
-///  .with_bucket_name(BUCKET_NAME)
-///  .build();
+/// let gcs = GoogleCloudStorageBuilder::from_env().with_bucket_name(BUCKET_NAME).build();
 /// ```
 #[derive(Debug, Clone)]
 pub struct GoogleCloudStorageBuilder {
