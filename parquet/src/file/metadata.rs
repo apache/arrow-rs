6296ada4cb23be0b2294482203e65d7e511ecfda
Ed Seidl <etseidl@live.com>
2024-02-15 21:18:41 -0800

    add new stats to chunk metadata test

--- parquet/src/file/metadata.rs
+++ parquet/src/file/metadata.rs
@@ -1240,6 +1240,9 @@ mod tests {
             .set_offset_index_length(Some(25))
             .set_column_index_offset(Some(8000))
             .set_column_index_length(Some(25))
+            .set_unencoded_byte_array_data_bytes(Some(2000))
+            .set_repetition_level_histogram(Some(vec![100,100]))
+            .set_definition_level_histogram(Some(vec![0,200]))
             .build()
             .unwrap();
 
