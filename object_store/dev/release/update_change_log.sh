30151220c29fa5e01365c2a4e153de01d5d2c041
Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>
2024-03-01 19:50:31 +1300

    Prepare object_store 0.9.1 (#5449)

--- object_store/dev/release/update_change_log.sh
+++ object_store/dev/release/update_change_log.sh
@@ -29,8 +29,8 @@
 
 set -e
 
-SINCE_TAG="object_store_0.8.0"
-FUTURE_RELEASE="object_store_0.9.0"
+SINCE_TAG="object_store_0.9.0"
+FUTURE_RELEASE="object_store_0.9.1"
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_TOP_DIR="$(cd "${SOURCE_DIR}/../../" && pwd)"
