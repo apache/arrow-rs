619d77e2456b72911b798d90975cdc406c006f68
Andrew Lamb <andrew@nerdnetworks.org>
2024-06-21 11:57:36 -0400

    Revert "Write Bloom filters between row groups instead of the end  (#5860)"
    This reverts commit 3930d5b056b8157e0b5c5eb01b1076e476acf99b.

--- parquet/src/arrow/async_writer/mod.rs
+++ parquet/src/arrow/async_writer/mod.rs
@@ -54,7 +54,7 @@ use crate::{
     arrow::arrow_writer::ArrowWriterOptions,
     arrow::ArrowWriter,
     errors::{ParquetError, Result},
-    file::{metadata::RowGroupMetaData, properties::WriterProperties},
+    file::{metadata::RowGroupMetaDataPtr, properties::WriterProperties},
     format::{FileMetaData, KeyValue},
 };
 use arrow_array::RecordBatch;
@@ -172,7 +172,7 @@ impl<W: AsyncFileWriter> AsyncArrowWriter<W> {
     }
 
     /// Returns metadata for any flushed row groups
-    pub fn flushed_row_groups(&self) -> &[RowGroupMetaData] {
+    pub fn flushed_row_groups(&self) -> &[RowGroupMetaDataPtr] {
         self.sync_writer.flushed_row_groups()
     }
 
