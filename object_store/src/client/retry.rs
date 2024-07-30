e796cf11bb48971da2e95141e6d2b620fd98b712
Kyle McCarthy <kyle@osmos.io>
2024-07-30 16:32:31 -0500

    fix(object_store): keep `retry::Error::error()` crate public

--- object_store/src/client/retry.rs
+++ object_store/src/client/retry.rs
@@ -79,7 +79,7 @@ impl Error {
         }
     }
 
-    pub fn error(self, store: &'static str, path: String) -> crate::Error {
+    pub(crate) fn error(self, store: &'static str, path: String) -> crate::Error {
         match self.status() {
             Some(StatusCode::NOT_FOUND) => crate::Error::NotFound {
                 path,
