52657a6d3130af4cd98792bad06df0cb782c18db
Costi Ciudatu <ciudatu@adobe.com>
2024-08-15 01:12:17 +0300

    Make the bearer token visible in FlightSqlServiceClient

--- arrow-flight/src/sql/client.rs
+++ arrow-flight/src/sql/client.rs
@@ -110,6 +110,11 @@ impl FlightSqlServiceClient<Channel> {
         self.token = None;
     }
 
+    /// Share the bearer token with potentially different `DoGet` clients
+    pub fn token(&self) -> &Option<String> {
+        &self.token
+    }
+
     /// Set header value.
     pub fn set_header(&mut self, key: impl Into<String>, value: impl Into<String>) {
         let key: String = key.into();
