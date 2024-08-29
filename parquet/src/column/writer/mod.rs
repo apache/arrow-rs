b4fb414e3ef0a6b85f21be8c7e7369fee8cc44f8
Ed Seidl <etseidl@live.com>
2024-08-28 17:55:06 -0700

    add some comments to test

--- parquet/src/column/writer/mod.rs
+++ parquet/src/column/writer/mod.rs
@@ -2670,6 +2670,7 @@ mod tests {
 
     #[test]
     fn test_column_index_with_null_pages() {
+        // write a single page of all nulls
         let page_writer = get_test_page_writer();
         let props = Default::default();
         let mut writer = get_test_column_writer::<Int32Type>(page_writer, 1, 0, props);
@@ -2678,12 +2679,17 @@ mod tests {
         let r = writer.close().unwrap();
         assert!(r.column_index.is_some());
         let col_idx = r.column_index.unwrap();
+        // null_pages should be true for page 0
         assert!(col_idx.null_pages[0]);
+        // min and max should be empty byte arrays
         assert_eq!(col_idx.min_values[0].len(), 0);
         assert_eq!(col_idx.max_values[0].len(), 0);
+        // null_counts should be defined and be 4 for page 0
         assert!(col_idx.null_counts.is_some());
         assert_eq!(col_idx.null_counts.as_ref().unwrap()[0], 4);
+        // there is no repetition so rep histogram should be absent
         assert!(col_idx.repetition_level_histograms.is_none());
+        // definition_level_histogram should be present and should be 0:4, 1:0
         assert!(col_idx.definition_level_histograms.is_some());
         assert_eq!(col_idx.definition_level_histograms.unwrap(), &[4, 0]);
     }
