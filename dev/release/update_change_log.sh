362e1a4f6dcda39eef18633d3209068bb60f3c85
Andrew Lamb <andrew@nerdnetworks.org>
2024-08-31 09:24:33 -0400

    Update changelog script

--- dev/release/update_change_log.sh
+++ dev/release/update_change_log.sh
@@ -29,8 +29,8 @@
 
 set -e
 
-SINCE_TAG="52.1.0"
-FUTURE_RELEASE="52.2.0"
+SINCE_TAG="52.2.0"
+FUTURE_RELEASE="53.0.0"
 
 SOURCE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 SOURCE_TOP_DIR="$(cd "${SOURCE_DIR}/../../" && pwd)"
