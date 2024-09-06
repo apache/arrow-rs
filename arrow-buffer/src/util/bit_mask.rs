be3076edbaf37347b135ec352e0ada2731495315
Kazuyuki Tanimura <ktanimura@apple.com>
2024-09-06 11:31:15 -0700

    address review comments

--- arrow-buffer/src/util/bit_mask.rs
+++ arrow-buffer/src/util/bit_mask.rs
@@ -55,6 +55,8 @@ fn set_upto_64bits(
     offset_read: usize,
     len: usize,
 ) -> (usize, usize) {
+    debug_assert!(offset_read + len <= data.len());
+    debug_assert!(offset_write + len <= write_data.len());
     let read_byte = offset_read / 8;
     let read_shift = offset_read % 8;
     let write_byte = offset_write / 8;
