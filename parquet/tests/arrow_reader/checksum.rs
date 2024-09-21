32f063a8d07992b3533346f13432b5aa4f841387
xmakro <makro@>
2024-09-21 00:27:26 -0700

    Add license

--- parquet/tests/arrow_reader/checksum.rs
+++ parquet/tests/arrow_reader/checksum.rs
@@ -1,3 +1,22 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+//! This file contains an end to end test for verifying checksums when reading parquet files.
+
 use std::path::PathBuf;
 
 use arrow::util::test_util::parquet_test_data;
@@ -26,7 +45,6 @@ fn test_datapage_v1_snappy_compressed_checksum() {
     let errors = read_file_batch_errors("datapage_v1-snappy-compressed-checksum.parquet");
     assert_eq!(errors, [Ok(()), Ok(()), Ok(()), Ok(()), Ok(())]);
 }
-
 
 #[test]
 fn test_plain_dict_uncompressed_checksum() {
