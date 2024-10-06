f2b9b466223c7a57d88924be9d0cb084056e1af4
dsgibbons <git@gibbons.foo>
2024-10-06 14:21:21 +1030

    chore: cargo fmt --all

--- parquet/src/file/properties.rs
+++ parquet/src/file/properties.rs
@@ -287,9 +287,9 @@ impl WriterProperties {
     /// Returns `coerce_types` boolean
     ///
     /// Some Arrow types do not have a corresponding Parquet logical type.
-    /// Affected Arrow data types include `Date64`, `Timestamp` and `Interval`. 
+    /// Affected Arrow data types include `Date64`, `Timestamp` and `Interval`.
     /// Writers have the option to coerce these into native Parquet types. Type
-    /// coercion allows for meaningful representations that do not require 
+    /// coercion allows for meaningful representations that do not require
     /// downstream readers to consider the embedded Arrow schema. However, type
     /// coercion also prevents the data from being round-tripped. This method
     /// returns `true` if type coercion enabled.
