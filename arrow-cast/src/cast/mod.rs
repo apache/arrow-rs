b53a8b4c9da643e6a38b1af5012dae8274b888f6
Curt Hagenlocher <curt@hagenlocher.org>
2024-10-19 10:15:04 -0700

    preliminary changes

--- arrow-cast/src/cast/mod.rs
+++ arrow-cast/src/cast/mod.rs
@@ -181,7 +181,7 @@ pub fn can_cast_types(from_type: &DataType, to_type: &DataType) -> bool {
             UInt8 | UInt16 | UInt32 | UInt64) |
         // decimal to signed numeric
         (Decimal32(_, _) | Decimal64(_, _) | Decimal128(_, _) | Decimal256(_, _),
-            Null | Int8 | Int16 | Int32 | Int64 | Float32 | Float64) |
+            Null | Int8 | Int16 | Int32 | Int64 | Float32 | Float64) => true,
         // decimal to string
         (Decimal32(_, _) | Decimal64(_, _) | Decimal128(_, _) | Decimal256(_, _),
             Utf8View | Utf8 | LargeUtf8) |
