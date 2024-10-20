cecc36184907257e123e5e24b78f39adfa088891
Andrew Lamb <andrew@nerdnetworks.org>
2024-10-20 07:52:27 -0400

    Fix `test_private_bucket` test

--- object_store/src/aws/resolve.rs
+++ object_store/src/aws/resolve.rs
@@ -80,13 +80,13 @@ mod tests {
 
     #[tokio::test]
     async fn test_private_bucket() {
-        let bucket = "bloxbender";
+        let bucket = "public-datasets";
 
         let region = resolve_bucket_region(bucket, &ClientOptions::new())
             .await
             .unwrap();
 
-        let expected = "us-west-2".to_string();
+        let expected = "eu-west-1".to_string();
 
         assert_eq!(region, expected);
     }
