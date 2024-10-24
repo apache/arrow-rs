a9294d7b06ce230f738c8bef25a1fd9a3b3e095c
Daniël Heres <danielheres@gmail.com>
2024-10-24 22:19:08 +0200

    Optimize `take_bits` / `take_boolean`: up to -25% (#6622)
    * Optimize take
    
    * Wip
    
    * Revert "Wip"
    
    This reverts commit 3846bfcddfebf0361c0b947cc2dda71c56af4e8c.
    
    * Revert "Revert "Wip""
    
    This reverts commit b6a59fc9b2329ee7b10d81992244f22ea0fc2d65.
    
    * Improve
    
    * Rev
    
    * Rev
    
    * Rev
    
    * Clippy
    
    * Fix
    
    * Style
    
    * Bring back take bytes optimization
    
    * Revert "Bring back take bytes optimization"
    
    This reverts commit 044908080758a2906b93da43d1a8039bab17b6ef.
    
    * Revert take_bytes change
    
    * Style

--- arrow-select/src/take.rs
+++ arrow-select/src/take.rs
@@ -424,22 +424,25 @@ fn take_bits<I: ArrowPrimitiveType>(
     indices: &PrimitiveArray<I>,
 ) -> BooleanBuffer {
     let len = indices.len();
-    let mut output_buffer = MutableBuffer::new_null(len);
-    let output_slice = output_buffer.as_slice_mut();
 
     match indices.nulls().filter(|n| n.null_count() > 0) {
-        Some(nulls) => nulls.valid_indices().for_each(|idx| {
-            if values.value(indices.value(idx).as_usize()) {
-                bit_util::set_bit(output_slice, idx);
-            }
-        }),
-        None => indices.values().iter().enumerate().for_each(|(i, index)| {
-            if values.value(index.as_usize()) {
-                bit_util::set_bit(output_slice, i);
-            }
-        }),
+        Some(nulls) => {
+            let mut output_buffer = MutableBuffer::new_null(len);
+            let output_slice = output_buffer.as_slice_mut();
+            nulls.valid_indices().for_each(|idx| {
+                if values.value(indices.value(idx).as_usize()) {
+                    bit_util::set_bit(output_slice, idx);
+                }
+            });
+            BooleanBuffer::new(output_buffer.into(), 0, len)
+        }
+        None => {
+            BooleanBuffer::collect_bool(len, |idx: usize| {
+                // SAFETY: idx<indices.len()
+                values.value(unsafe { indices.value_unchecked(idx).as_usize() })
+            })
+        }
     }
-    BooleanBuffer::new(output_buffer.into(), 0, indices.len())
 }
 
 /// `take` implementation for boolean arrays
