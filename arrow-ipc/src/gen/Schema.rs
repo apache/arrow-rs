43296c703ea1b3854a6646859258c5cfb6716989
Benjamin Kietzman <bengilgit@gmail.com>
2024-10-25 12:30:19 -0500

    apply a patch to generated code rather than requiring patched flatc

--- arrow-ipc/src/gen/Schema.rs
+++ arrow-ipc/src/gen/Schema.rs
@@ -1152,6 +1152,10 @@ impl<'b> flatbuffers::Push for Buffer {
         let src = ::core::slice::from_raw_parts(self as *const Buffer as *const u8, Self::size());
         dst.copy_from_slice(src);
     }
+    #[inline]
+    fn alignment() -> flatbuffers::PushAlignment {
+        flatbuffers::PushAlignment::new(8)
+    }
 }
 
 impl<'a> flatbuffers::Verifiable for Buffer {
