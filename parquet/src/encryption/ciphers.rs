e53306e6e93ee12cd8744cc1f5f50f4eaf653399
Rok Mihevc <rok@mihevc.org>
2025-01-21 17:59:44 +0100

    Cleanup

--- parquet/src/encryption/ciphers.rs
+++ parquet/src/encryption/ciphers.rs
@@ -175,7 +175,7 @@ pub fn create_footer_aad(file_aad: &[u8]) -> Result<Vec<u8>> {
     create_module_aad(file_aad, ModuleType::Footer, 0, 0, None)
 }
 
-pub fn create_page_aad(file_aad: &[u8], module_type: ModuleType, row_group_ordinal: usize,
+pub(crate) fn create_page_aad(file_aad: &[u8], module_type: ModuleType, row_group_ordinal: usize,
                         column_ordinal: usize, page_ordinal: Option<usize>) -> Result<Vec<u8>> {
     create_module_aad(file_aad, module_type, row_group_ordinal, column_ordinal, page_ordinal)
 }
