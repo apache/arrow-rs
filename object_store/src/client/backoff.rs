a292c78b80929e222119cff6f71f94a7fae02c52
Matthijs Brobbel <m1brobbel@gmail.com>
2025-01-29 12:25:08 +0100

    Rustfmt

--- object_store/src/client/backoff.rs
+++ object_store/src/client/backoff.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use rand::{rng, prelude::*};
+use rand::{prelude::*, rng};
 use std::time::Duration;
 
 /// Exponential backoff with decorrelated jitter algorithm
