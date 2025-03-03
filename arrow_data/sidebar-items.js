ac128cda67c8b1afd1b905889f2ae73de71c9e94
tustvold <tustvold@users.noreply.github.com>
2025-03-03 18:17:42 +0000

    deploy: 3674073bba31e082b337cee63726cfeb186a3a0c

---
+++ arrow_data/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BufferSpec"],"fn":["layout","validate_binary_view","validate_string_view"],"mod":["bit_iterator","bit_mask","byte_view","data","decimal","equal","ffi","transform"],"struct":["ArrayData","ArrayDataBuilder","ByteView","DataTypeLayout"],"type":["ArrayDataRef"]};
