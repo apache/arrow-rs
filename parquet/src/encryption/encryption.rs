407063e0106e63b246fffb0381d6e36335b04804
Adam Reeve <adreeve@gmail.com>
2025-03-04 09:59:26 +1300

    Add key metadata to file encryption properties

--- parquet/src/encryption/encryption.rs
+++ parquet/src/encryption/encryption.rs
@@ -19,14 +19,34 @@ use std::collections::HashMap;
 use std::io::Write;
 use thrift::protocol::TCompactOutputProtocol;
 use crate::encryption::ciphers::{BlockEncryptor, RingGcmBlockEncryptor};
+use crate::errors::Result;
 use crate::file::writer::TrackedWrite;
 use crate::thrift::TSerializable;
 
 #[derive(Debug, Clone, PartialEq)]
+pub struct EncryptionKey {
+    key: Vec<u8>,
+    key_metadata: Option<Vec<u8>>
+}
+
+impl EncryptionKey {
+    pub fn new(key: Vec<u8>) -> EncryptionKey {
+        Self {
+            key,
+            key_metadata: None,
+        }
+    }
+
+    pub fn set_metadata(&mut self, metadata: Vec<u8>) {
+        self.key_metadata = Some(metadata);
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
 pub struct FileEncryptionProperties {
     encrypt_footer: bool,
-    footer_key: Vec<u8>,
-    column_keys: Option<HashMap<Vec<u8>, Vec<u8>>>,
+    footer_key: EncryptionKey,
+    column_keys: HashMap<Vec<u8>, EncryptionKey>,
     pub(crate) aad_prefix: Option<Vec<u8>>,
 }
 
@@ -41,23 +61,35 @@ impl FileEncryptionProperties {
 }
 
 pub struct EncryptionPropertiesBuilder {
-    footer_key: Vec<u8>,
-    column_keys: Option<HashMap<Vec<u8>, Vec<u8>>>,
+    footer_key: EncryptionKey,
+    column_keys: HashMap<Vec<u8>, EncryptionKey>,
     aad_prefix: Option<Vec<u8>>,
+    encrypt_footer: bool,
 }
 
 impl EncryptionPropertiesBuilder {
     pub fn new(footer_key: Vec<u8>) -> EncryptionPropertiesBuilder {
         Self {
-            footer_key,
-            column_keys: None,
+            footer_key: EncryptionKey::new(footer_key),
+            column_keys: HashMap::default(),
             aad_prefix: None,
+            encrypt_footer: true,
         }
     }
 
-    pub fn build(self) -> crate::errors::Result<FileEncryptionProperties> {
+    pub fn with_plaintext_footer(mut self, plaintext_footer: bool) -> Self {
+        self.encrypt_footer = !plaintext_footer;
+        self
+    }
+
+    pub fn with_footer_key_metadata(mut self, metadata: Vec<u8>) -> Self {
+        self.footer_key.set_metadata(metadata);
+        self
+    }
+
+    pub fn build(self) -> Result<FileEncryptionProperties> {
         Ok(FileEncryptionProperties {
-            encrypt_footer: true,
+            encrypt_footer: self.encrypt_footer,
             footer_key: self.footer_key,
             column_keys: self.column_keys,
             aad_prefix: self.aad_prefix,
@@ -90,7 +122,7 @@ impl FileEncryptor {
     // }
     // }
     pub(crate) fn get_footer_encryptor(&self) -> RingGcmBlockEncryptor {
-        RingGcmBlockEncryptor::new(&self.file_encryption_properties.footer_key.clone())
+        RingGcmBlockEncryptor::new(&self.file_encryption_properties.footer_key.key.clone())
     }
 }
 
@@ -98,7 +130,7 @@ pub(crate) fn encrypt_object<T: TSerializable, W: Write>(
     object: T,
     file_encryption_properties: &FileEncryptionProperties,
     sink: &mut TrackedWrite<W>,
-) -> Result<(), crate::errors::ParquetError> {
+) -> Result<()> {
     // todo: encrypt
     let mut buffer: Vec<u8> = vec![];
     {
