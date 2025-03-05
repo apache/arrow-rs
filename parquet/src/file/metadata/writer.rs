877805adf00d39d5868216a2a7c69add0dd2cb9a
Rok Mihevc <rok@mihevc.org>
2025-03-05 02:22:33 +0100

    clippy

--- parquet/src/file/metadata/writer.rs
+++ parquet/src/file/metadata/writer.rs
@@ -277,7 +277,9 @@ impl<'a, W: Write> ThriftMetadataWriter<'a, W> {
         file_encryptor: &Arc<FileEncryptor>,
     ) -> Result<crate::format::FileCryptoMetaData> {
         let properties = file_encryptor.properties();
-        let supply_aad_prefix = properties.aad_prefix().map(|_| !properties.store_aad_prefix());
+        let supply_aad_prefix = properties
+            .aad_prefix()
+            .map(|_| !properties.store_aad_prefix());
         let encryption_algorithm = AesGcmV1 {
             aad_prefix: properties.aad_prefix().cloned(),
             aad_file_unique: Some(file_encryptor.aad_file_unique().clone()),
