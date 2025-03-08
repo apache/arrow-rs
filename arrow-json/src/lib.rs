1202314d013d62a11db1533f1438b3cbca4741f5
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2025-03-08 11:32:09 -0600

    Update lib.rs
    Co-authored-by: Raphael Taylor-Davies <1781103+tustvold@users.noreply.github.com>

--- arrow-json/src/lib.rs
+++ arrow-json/src/lib.rs
@@ -71,7 +71,7 @@ pub mod writer;
 
 pub use self::reader::{Reader, ReaderBuilder};
 pub use self::writer::{
-    ArrayWriter, Encoder, EncoderFactory, LineDelimitedWriter, Writer, WriterBuilder,
+    ArrayWriter, Encoder, EncoderOptions, EncoderFactory, LineDelimitedWriter, Writer, WriterBuilder,
 };
 use half::f16;
 use serde_json::{Number, Value};
