6737ebe10f34144bcbd820bf8e55aec18e61f0fa
Weston Pace <weston.pace@gmail.com>
2025-04-03 06:55:02 -0700

    Use ArrayData::slice instead of re-inventing the wheel

--- arrow-array/src/array/struct_array.rs
+++ arrow-array/src/array/struct_array.rs
@@ -301,26 +301,11 @@ impl From<ArrayData> for StructArray {
             .child_data()
             .iter()
             .map(|cd| {
-                let child_offset = cd.offset();
-                let child_len = cd.len();
-                assert!(
-                    child_len >= parent_len + parent_offset,
-                    "struct array has offset {} and len {} but child array only has {} items",
-                    parent_offset,
-                    parent_len,
-                    child_len
-                );
-                // SAFETY: We have already checked that the child array has enough items and the
-                // only thing we are changing is the offset and length.  As long as the child data
-                // was previously valid, then the new child data is also valid.
-                let cd = unsafe {
-                    cd.clone()
-                        .into_builder()
-                        .offset(child_offset + parent_offset)
-                        .len(parent_len)
-                        .build_unchecked()
-                };
-                make_array(cd.clone())
+                if parent_offset != 0 || cd.len() != parent_len {
+                    make_array(cd.slice(parent_offset, parent_len))
+                } else {
+                    make_array(cd.clone())
+                }
             })
             .collect();
 
@@ -592,9 +577,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(
-        expected = "struct array has offset 3 and len 3 but child array only has 5 items"
-    )]
+    #[should_panic(expected = "assertion failed: (offset + length) <= self.len()")]
     fn test_struct_array_from_data_with_offset_and_length_error() {
         let int_arr = Int32Array::from(vec![1, 2, 3, 4, 5]);
         let int_field = Field::new("x", DataType::Int32, false);
