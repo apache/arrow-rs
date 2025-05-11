1a1e6b6a3ff25cc5adec5b9aa2f6615159609663
Jonas Irgens Kylling <jonas@dune.com>
2025-05-11 20:12:41 +0200

    Simplify RowNumberReader with iterators
    Co-authored-by: scovich <scovich@users.noreply.github.com>

--- parquet/src/arrow/array_reader/row_number.rs
+++ parquet/src/arrow/array_reader/row_number.rs
@@ -21,28 +21,45 @@ use crate::file::metadata::RowGroupMetaData;
 use arrow_array::{ArrayRef, Int64Array};
 use arrow_schema::DataType;
 use std::any::Any;
-use std::collections::VecDeque;
 use std::sync::Arc;
 
 pub(crate) struct RowNumberReader {
-    row_numbers: Vec<i64>,
-    row_groups: RowGroupSizeIterator,
+    buffered_row_numbers: Vec<i64>,
+    remaining_row_numbers: std::iter::Flatten<std::vec::IntoIter<std::ops::Range<i64>>>,
 }
 
 impl RowNumberReader {
-    pub(crate) fn try_new<I>(row_groups: impl IntoIterator<Item = I>) -> Result<Self>
-    where
-        I: TryInto<RowGroupSize, Error = ParquetError>,
-    {
-        let row_groups = RowGroupSizeIterator::try_new(row_groups)?;
+    pub(crate) fn try_new<'a>(
+        row_groups: impl Iterator<Item = &'a RowGroupMetaData>,
+    ) -> Result<Self> {
+        let ranges = row_groups
+            .map(|rg| {
+                let first_row_number = rg.first_row_index().ok_or(ParquetError::General(
+                    "Row group missing row number".to_string(),
+                ))?;
+                Ok(first_row_number..first_row_number + rg.num_rows())
+            })
+            .collect::<Result<Vec<_>>>()?;
         Ok(Self {
-            row_numbers: Vec::new(),
-            row_groups,
+            buffered_row_numbers: Vec::new(),
+            remaining_row_numbers: ranges.into_iter().flatten(),
         })
     }
 }
 
 impl ArrayReader for RowNumberReader {
+    fn read_records(&mut self, batch_size: usize) -> Result<usize> {
+        let starting_len = self.buffered_row_numbers.len();
+        self.buffered_row_numbers
+            .extend((&mut self.remaining_row_numbers).take(batch_size));
+        Ok(self.buffered_row_numbers.len() - starting_len)
+    }
+
+    fn skip_records(&mut self, num_records: usize) -> Result<usize> {
+        // TODO: Use advance_by when it stabilizes to improve performance
+        Ok((&mut self.remaining_row_numbers).take(num_records).count())
+    }
+
     fn as_any(&self) -> &dyn Any {
         self
     }
@@ -51,20 +68,10 @@ impl ArrayReader for RowNumberReader {
         &DataType::Int64
     }
 
-    fn read_records(&mut self, batch_size: usize) -> Result<usize> {
-        let read = self
-            .row_groups
-            .read_records(batch_size, &mut self.row_numbers);
-        Ok(read)
-    }
-
     fn consume_batch(&mut self) -> Result<ArrayRef> {
-        Ok(Arc::new(Int64Array::from_iter(self.row_numbers.drain(..))))
-    }
-
-    fn skip_records(&mut self, num_records: usize) -> Result<usize> {
-        let skipped = self.row_groups.skip_records(num_records);
-        Ok(skipped)
+        Ok(Arc::new(Int64Array::from_iter(
+            self.buffered_row_numbers.drain(..),
+        )))
     }
 
     fn get_def_levels(&self) -> Option<&[i16]> {
@@ -73,82 +80,5 @@ impl ArrayReader for RowNumberReader {
 
     fn get_rep_levels(&self) -> Option<&[i16]> {
         None
-    }
-}
-
-struct RowGroupSizeIterator {
-    row_groups: VecDeque<RowGroupSize>,
-}
-
-impl RowGroupSizeIterator {
-    fn try_new<I>(row_groups: impl IntoIterator<Item = I>) -> Result<Self>
-    where
-        I: TryInto<RowGroupSize, Error = ParquetError>,
-    {
-        Ok(Self {
-            row_groups: VecDeque::from(
-                row_groups
-                    .into_iter()
-                    .map(TryInto::try_into)
-                    .collect::<Result<Vec<_>>>()?,
-            ),
-        })
-    }
-}
-
-impl RowGroupSizeIterator {
-    fn read_records(&mut self, mut batch_size: usize, row_numbers: &mut Vec<i64>) -> usize {
-        let mut read = 0;
-        while batch_size > 0 {
-            let Some(front) = self.row_groups.front_mut() else {
-                return read as usize;
-            };
-            let to_read = std::cmp::min(front.num_rows, batch_size as i64);
-            row_numbers.extend(front.first_row_number..front.first_row_number + to_read);
-            front.num_rows -= to_read;
-            front.first_row_number += to_read;
-            if front.num_rows == 0 {
-                self.row_groups.pop_front();
-            }
-            batch_size -= to_read as usize;
-            read += to_read;
-        }
-        read as usize
-    }
-
-    fn skip_records(&mut self, mut num_records: usize) -> usize {
-        let mut skipped = 0;
-        while num_records > 0 {
-            let Some(front) = self.row_groups.front_mut() else {
-                return skipped as usize;
-            };
-            let to_skip = std::cmp::min(front.num_rows, num_records as i64);
-            front.num_rows -= to_skip;
-            front.first_row_number += to_skip;
-            if front.num_rows == 0 {
-                self.row_groups.pop_front();
-            }
-            skipped += to_skip;
-            num_records -= to_skip as usize;
-        }
-        skipped as usize
-    }
-}
-
-pub(crate) struct RowGroupSize {
-    first_row_number: i64,
-    num_rows: i64,
-}
-
-impl TryFrom<&RowGroupMetaData> for RowGroupSize {
-    type Error = ParquetError;
-
-    fn try_from(rg: &RowGroupMetaData) -> Result<Self, Self::Error> {
-        Ok(Self {
-            first_row_number: rg.first_row_index().ok_or(ParquetError::General(
-                "Row group missing row number".to_string(),
-            ))?,
-            num_rows: rg.num_rows(),
-        })
     }
 }
