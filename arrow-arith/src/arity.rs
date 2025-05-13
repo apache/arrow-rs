3d9c02f48ca3da95624245c0808d825184a2a20d
Daniël Heres <danielheres@gmail.com>
2025-05-13 11:48:17 +0200

    Cleanup

--- arrow-arith/src/arity.rs
+++ arrow-arith/src/arity.rs
@@ -21,7 +21,7 @@ use arrow_array::builder::BufferBuilder;
 use arrow_array::*;
 use arrow_buffer::buffer::NullBuffer;
 use arrow_buffer::ArrowNativeType;
-use arrow_buffer::{Buffer, MutableBuffer};
+use arrow_buffer::MutableBuffer;
 use arrow_data::ArrayData;
 use arrow_schema::ArrowError;
 
