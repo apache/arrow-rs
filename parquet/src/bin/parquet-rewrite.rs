3762668d95622ba80c69edaca18c6c0b339d776e
Jigao Luo <jigao.luo@outlook.com>
2025-05-31 15:59:20 +0200

    Adding Encoding argument in parquet-rewrite
    Signed-off-by: Jigao Luo <jigao.luo@outlook.com>

--- parquet/src/bin/parquet-rewrite.rs
+++ parquet/src/bin/parquet-rewrite.rs
@@ -39,7 +39,7 @@ use arrow_array::RecordBatchReader;
 use clap::{builder::PossibleValue, Parser, ValueEnum};
 use parquet::{
     arrow::{arrow_reader::ParquetRecordBatchReaderBuilder, ArrowWriter},
-    basic::Compression,
+    basic::{Compression, Encoding},
     file::{
         properties::{BloomFilterPosition, EnabledStatistics, WriterProperties, WriterVersion},
         reader::FileReader,
@@ -85,6 +85,53 @@ impl From<CompressionArgs> for Compression {
             CompressionArgs::Lz4 => Self::LZ4,
             CompressionArgs::Zstd => Self::ZSTD(Default::default()),
             CompressionArgs::Lz4Raw => Self::LZ4_RAW,
+        }
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum EncodingArgs {
+    /// Default byte encoding.
+    Plain,
+
+    /// **Deprecated** dictionary encoding.
+    PlainDictionary,
+
+    /// Group packed run length encoding.
+    Rle,
+
+    /// **Deprecated** Bit-packed encoding.
+    BitPacked,
+
+    /// Delta encoding for integers, either INT32 or INT64.
+    DeltaBinaryPacked,
+
+    /// Encoding for byte arrays to separate the length values and the data.
+    DeltaLengthByteArray,
+
+    /// Incremental encoding for byte arrays.
+    DeltaByteArray,
+
+    /// Dictionary encoding.
+    RleDictionary,
+
+    /// Encoding for fixed-width data.
+    ByteStreamSplit,
+}
+
+#[allow(deprecated)]
+impl From<EncodingArgs> for Encoding {
+    fn from(value: EncodingArgs) -> Self {
+        match value {
+            EncodingArgs::Plain => Self::PLAIN,
+            EncodingArgs::PlainDictionary => Self::PLAIN_DICTIONARY,
+            EncodingArgs::Rle => Self::RLE,
+            EncodingArgs::BitPacked => Self::BIT_PACKED,
+            EncodingArgs::DeltaBinaryPacked => Self::DELTA_BINARY_PACKED,
+            EncodingArgs::DeltaLengthByteArray => Self::DELTA_LENGTH_BYTE_ARRAY,
+            EncodingArgs::DeltaByteArray => Self::DELTA_BYTE_ARRAY,
+            EncodingArgs::RleDictionary => Self::RLE_DICTIONARY,
+            EncodingArgs::ByteStreamSplit => Self::BYTE_STREAM_SPLIT,
         }
     }
 }
@@ -168,9 +215,13 @@ struct Args {
     #[clap(short, long)]
     output: String,
 
-    /// Compression used.
+    /// Compression used for any column.
     #[clap(long, value_enum)]
     compression: Option<CompressionArgs>,
+
+    /// Encoding used for any column, if dictionary is not enabled.
+    #[clap(long, value_enum)]
+    encoding: Option<EncodingArgs>,
 
     /// Sets maximum number of rows in a row group.
     #[clap(long)]
@@ -252,6 +303,19 @@ fn main() {
     if let Some(value) = args.compression {
         writer_properties_builder = writer_properties_builder.set_compression(value.into());
     }
+    {
+        // setup encoding
+        if let Some(value) = args.dictionary_enabled {
+            writer_properties_builder = writer_properties_builder.set_dictionary_enabled(value);
+        }
+        if let Some(value) = args.dictionary_page_size_limit {
+            writer_properties_builder =
+                writer_properties_builder.set_dictionary_page_size_limit(value);
+        }
+        if let Some(value) = args.encoding {
+            writer_properties_builder = writer_properties_builder.set_encoding(value.into());
+        }
+    }
     if let Some(value) = args.max_row_group_size {
         writer_properties_builder = writer_properties_builder.set_max_row_group_size(value);
     }
@@ -260,9 +324,6 @@ fn main() {
     }
     if let Some(value) = args.data_page_size_limit {
         writer_properties_builder = writer_properties_builder.set_data_page_size_limit(value);
-    }
-    if let Some(value) = args.dictionary_page_size_limit {
-        writer_properties_builder = writer_properties_builder.set_dictionary_page_size_limit(value);
     }
     #[allow(deprecated)]
     if let Some(value) = args.max_statistics_size {
@@ -283,9 +344,6 @@ fn main() {
                     writer_properties_builder.set_bloom_filter_position(value.into());
             }
         }
-    }
-    if let Some(value) = args.dictionary_enabled {
-        writer_properties_builder = writer_properties_builder.set_dictionary_enabled(value);
     }
     if let Some(value) = args.statistics_enabled {
         writer_properties_builder = writer_properties_builder.set_statistics_enabled(value.into());
