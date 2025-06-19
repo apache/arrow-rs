a291e4b593c43b0050e003aecb18bd4c54f7d30b
zhuqi-lucas <821684824@qq.com>
2025-06-19 17:20:50 +0800

    fmt

--- parquet/src/file/writer.rs
+++ parquet/src/file/writer.rs
@@ -395,7 +395,7 @@ impl<W: Write + Send> SerializedFileWriter<W> {
     }
 
     /// Writes the given buf bytes to the internal buffer.
-    pub fn write_all(&mut self, buf :&[u8]) -> std::io::Result<()> {
+    pub fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()> {
         self.buf.write_all(buf)
     }
 
