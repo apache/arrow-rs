04f0669d395ad39ec14baffa07a85e57d1bbe590
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2025-07-13 12:00:02 -0400

    Reserve capacity beforehand

--- parquet-variant/src/builder.rs
+++ parquet-variant/src/builder.rs
@@ -402,6 +402,11 @@ impl<S: AsRef<str>> FromIterator<S> for MetadataBuilder {
 
 impl<S: AsRef<str>> Extend<S> for MetadataBuilder {
     fn extend<T: IntoIterator<Item = S>>(&mut self, iter: T) {
+        let iter = iter.into_iter();
+        let (min, max) = iter.size_hint();
+
+        self.field_names.reserve(max.unwrap_or(min));
+
         for field_name in iter {
             self.upsert_field_name(field_name.as_ref());
         }
@@ -756,6 +761,15 @@ impl VariantBuilder {
     /// reading [`Variant`]s.
     pub fn with_field_names<'a>(mut self, field_names: impl Iterator<Item = &'a str>) -> Self {
         self.metadata_builder.extend(field_names);
+
+        self
+    }
+
+    /// This method reserves capacity for field names in the Variant metadata,
+    /// which can improve performance when you know the approximate number of unique field
+    /// names that will be used across all objects in the [`Variant`].
+    pub fn with_field_name_capacity(mut self, capacity: usize) -> Self {
+        self.metadata_builder.field_names.reserve(capacity);
 
         self
     }
