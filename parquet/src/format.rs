9452737c7fdcdf57279862d4387fe1b70c3271f5
Ed Seidl <etseidl@live.com>
2025-07-15 13:35:28 -0700

    further simplified

--- parquet/src/format.rs
+++ parquet/src/format.rs
@@ -4987,44 +4987,13 @@ impl crate::thrift::TSerializable for TypeDefinedOrder {
 #[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct ColumnOrderDisc(i32);
 
-impl ColumnOrderDisc {
-  pub const TYPE_ORDER : Self = Self(1);
-  pub const NO_SUCH_ORDER: Self = Self(2);
-  pub const ENUM_VALUES: &'static [Self] = &[
-    Self::TYPE_ORDER,
-  ];
-}
-
-impl From<i32> for ColumnOrderDisc {
-  fn from(i: i32) -> Self {
-    match i {
-      1 => ColumnOrderDisc::TYPE_ORDER,
-      2 => ColumnOrderDisc::NO_SUCH_ORDER,
-      _ => ColumnOrderDisc(i)
-    }
+impl Default for ColumnOrderDisc {
+  fn default() -> Self {
+    Self(0)
   }
 }
 
-impl From<&i32> for ColumnOrderDisc {
-  fn from(i: &i32) -> Self {
-    ColumnOrderDisc::from(*i)
-  }
-}
-
-impl From<ColumnOrderDisc> for i32 {
-  fn from(e: ColumnOrderDisc) -> i32 {
-    e.0
-  }
-}
-
-impl From<&ColumnOrderDisc> for i32 {
-  fn from(e: &ColumnOrderDisc) -> i32 {
-    e.0
-  }
-}
-
-
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 #[allow(non_snake_case)]
 pub struct ColumnOrder {
   pub disc: ColumnOrderDisc,
@@ -5034,21 +5003,8 @@ pub struct ColumnOrder {
 
 #[allow(non_snake_case)]
 impl ColumnOrder {
-  pub const TYPE_ORDER : ColumnOrderDisc = ColumnOrderDisc::TYPE_ORDER;
-  pub const NO_SUCH_ORDER : ColumnOrderDisc = ColumnOrderDisc::NO_SUCH_ORDER;
-
-  pub fn new<F1, F2, F3>(disc: F1, TYPE_ORDER: F2, NO_SUCH_ORDER: F3) -> Self
-  where
-    F1: Into<ColumnOrderDisc>,
-    F2: Into<Option<TypeDefinedOrder>>,
-    F3: Into<Option<TypeDefinedOrder>>
-  {
-    Self {
-      disc: disc.into(),
-      TYPE_ORDER: TYPE_ORDER.into(),
-      NO_SUCH_ORDER: NO_SUCH_ORDER.into(),
-    }
-  }
+  pub const TYPE_ORDER : ColumnOrderDisc = ColumnOrderDisc(1);
+  pub const NO_SUCH_ORDER : ColumnOrderDisc = ColumnOrderDisc(2);
 
   pub fn newTYPE_ORDER() -> Self {
     Self {
@@ -5067,16 +5023,10 @@ impl ColumnOrder {
   }
 }
 
-impl Default for ColumnOrder {
-  fn default() -> ColumnOrder {
-    ColumnOrder::new(0, None, None)
-  }
-}
-
 impl crate::thrift::TSerializable for ColumnOrder {
   fn read_from_in_protocol<T: TInputProtocol>(i_prot: &mut T) -> thrift::Result<ColumnOrder> {
     let mut received_field_count = 0;
-    let mut disc : ColumnOrderDisc = ColumnOrderDisc(0);
+    let mut disc : ColumnOrderDisc = Default::default();
     let mut f_1: Option<TypeDefinedOrder> = None;
     let mut f_2: Option<TypeDefinedOrder> = None;
     i_prot.read_struct_begin()?;
@@ -5090,13 +5040,13 @@ impl crate::thrift::TSerializable for ColumnOrder {
         1 => {
           let val = TypeDefinedOrder::read_from_in_protocol(i_prot)?;
           f_1 = Some(val);
-          disc = ColumnOrderDisc::TYPE_ORDER;
+          disc = ColumnOrder::TYPE_ORDER;
           received_field_count += 1;
         },
         2 => {
           let val = TypeDefinedOrder::read_from_in_protocol(i_prot)?;
           f_2 = Some(val);
-          disc = ColumnOrderDisc::NO_SUCH_ORDER;
+          disc = ColumnOrder::NO_SUCH_ORDER;
           received_field_count += 1;
         },
         _ => {
@@ -5138,14 +5088,14 @@ impl crate::thrift::TSerializable for ColumnOrder {
     let struct_ident = TStructIdentifier::new("ColumnOrder");
     o_prot.write_struct_begin(&struct_ident)?;
     match self.disc {
-      ColumnOrderDisc::TYPE_ORDER => {
+      ColumnOrder::TYPE_ORDER => {
         if let Some(ref fld_var) = self.TYPE_ORDER {
           o_prot.write_field_begin(&TFieldIdentifier::new("TYPE_ORDER", TType::Struct, 1))?;
           fld_var.write_to_out_protocol(o_prot)?;
           o_prot.write_field_end()?
         }
       },
-      ColumnOrderDisc::NO_SUCH_ORDER => {
+      ColumnOrder::NO_SUCH_ORDER => {
         if let Some(ref fld_var) = self.NO_SUCH_ORDER {
           o_prot.write_field_begin(&TFieldIdentifier::new("NO_SUCH_ORDER", TType::Struct, 2))?;
           fld_var.write_to_out_protocol(o_prot)?;
