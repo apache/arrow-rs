4ec17d6a54bb08e0e51340479152cbeb37dc9eab
Jake Dern <33842784+JakeDern@users.noreply.github.com>
2025-08-08 11:47:56 -0700

    Update arrow-ipc/src/reader.rs
    Co-authored-by: Liang-Chi Hsieh <viirya@gmail.com>

--- arrow-ipc/src/reader.rs
+++ arrow-ipc/src/reader.rs
@@ -1486,11 +1486,8 @@ impl<R: Read> StreamReader<R> {
             i32::from_le_bytes(meta_size)
         };
 
-        let Ok(meta_len) = usize::try_from(meta_len) else {
-            return Err(ArrowError::ParseError(format!(
-                "Invalid metadata length: {meta_len}"
-            )));
-        };
+        let meta_len = usize::try_from(meta_len)
+            .map_err(|_| ArrowError::ParseError(format!("Invalid metadata length: {meta_len}")))?;
 
         if meta_len == 0 {
             // the stream has ended, mark the reader as finished
