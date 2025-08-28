18194d60f4538d2070d095b1e2932556b1cbc5ae
Marco Neumann <marco@crepererum.net>
2025-08-28 11:14:52 +0200

    feat: `SSLKEYLOGFILE` support for flight CLI

--- arrow-flight/src/bin/flight_sql_client.rs
+++ arrow-flight/src/bin/flight_sql_client.rs
@@ -85,6 +85,14 @@ struct ClientArgs {
     #[clap(long)]
     tls: bool,
 
+    /// Dump TLS key log.
+    ///
+    /// The target file is specified by the `SSLKEYLOGFILE` environment variable.
+    ///
+    /// Requires `--tls`.
+    #[clap(long, requires = "tls")]
+    key_log: bool,
+
     /// Server host.
     ///
     /// Required.
@@ -357,7 +365,11 @@ async fn setup_client(args: ClientArgs) -> Result<FlightSqlServiceClient<Channel
         .keep_alive_while_idle(true);
 
     if args.tls {
-        let tls_config = ClientTlsConfig::new().with_enabled_roots();
+        let mut tls_config = ClientTlsConfig::new().with_enabled_roots();
+        if args.key_log {
+            tls_config = tls_config.use_key_log();
+        }
+
         endpoint = endpoint
             .tls_config(tls_config)
             .context("create TLS endpoint")?;
