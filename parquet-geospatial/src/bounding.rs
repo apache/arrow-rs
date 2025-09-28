2cfa1994e0b179e794787b5b4f0de675fed36f10
Dewey Dunnington <dewey@wherobots.com>
2025-09-27 21:23:18 -0500

    fix suggestion

--- parquet-geospatial/src/bounding.rs
+++ parquet-geospatial/src/bounding.rs
@@ -145,7 +145,7 @@ impl GeometryBounder {
     /// by this bounder. These identifiers are ISO WKB identifiers (e.g., 1001
     /// for PointZ). The output is always returned sorted.
     pub fn geometry_types(&self) -> Vec<i32> {
-        let mut out = self.geometry_types.iter().copied().collect();
+        let mut out = self.geometry_types.iter().copied().collect::<Vec<_>>();
         out.sort();
         out
     }
