822d74a50d06a9959e350b63cb4300a7de8669a3
Rok Mihevc <rok@mihevc.org>
2025-12-03 04:33:28 +0100

    clippy

--- parquet/src/file/metadata/thrift/mod.rs
+++ parquet/src/file/metadata/thrift/mod.rs
@@ -1459,7 +1459,7 @@ fn should_write_column_stats(column_chunk: &ColumnChunkMetaData) -> bool {
 }
 
 #[cfg(not(feature = "encryption"))]
-fn should_write_column_stats(column_chunk: &ColumnChunkMetaData) -> bool {
+fn should_write_column_stats(_column_chunk: &ColumnChunkMetaData) -> bool {
     true
 }
 
