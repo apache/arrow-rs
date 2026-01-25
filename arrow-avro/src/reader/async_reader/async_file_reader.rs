61c44e4e28a65783892525562c0c01caee186273
Emily Matheys <emilymatt96@gmail.com>
2026-01-25 18:33:10 +0200

    fix various failures

--- arrow-avro/src/reader/async_reader/async_file_reader.rs
+++ arrow-avro/src/reader/async_reader/async_file_reader.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use arrow_schema::ArrowError;
 use bytes::Bytes;
 use futures::FutureExt;
@@ -6,14 +23,14 @@ use std::io::SeekFrom;
 use std::ops::Range;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncSeek, AsyncSeekExt};
 
-/// The asynchronous interface used by [`AsyncAvroFileReader`] to read avro files
+/// The asynchronous interface used by [`super::AsyncAvroFileReader`] to read avro files
 ///
 /// Notes:
 ///
 /// 1. There is a default implementation for types that implement [`AsyncRead`]
 ///    and [`AsyncSeek`], for example [`tokio::fs::File`].
 ///
-/// 2. [`AvroObjectReader`], available when the `object_store` crate feature
+/// 2. [`super::AvroObjectReader`], available when the `object_store` crate feature
 ///    is enabled, implements this interface for [`ObjectStore`].
 ///
 /// [`ObjectStore`]: object_store::ObjectStore
