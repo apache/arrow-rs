d64f8be588a8b0166be40be888733746825c3382
Vignesh <149236000+vigneshsiva11@users.noreply.github.com>
2026-02-05 15:11:19 +0000

    Format Parquet large string overflow tests

--- parquet/tests/arrow_reader/large_string_overflow.rs
+++ parquet/tests/arrow_reader/large_string_overflow.rs
@@ -14,7 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 use std::sync::Arc;
 
 use arrow_array::{ArrayRef, BinaryArray, RecordBatch};
