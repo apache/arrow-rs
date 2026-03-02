285baaa60136019ad59ec8bcbbd1929154f259c7
Liam Bao <liam.zw.bao@gmail.com>
2026-03-01 20:57:05 -0500

    Move IS_VIEW check out of the loop

--- arrow-json/src/reader/list_array.rs
+++ arrow-json/src/reader/list_array.rs
@@ -18,8 +18,8 @@
 use crate::reader::tape::{Tape, TapeElement};
 use crate::reader::{ArrayDecoder, DecoderContext};
 use arrow_array::OffsetSizeTrait;
-use arrow_array::builder::{BooleanBufferBuilder, BufferBuilder};
-use arrow_buffer::buffer::NullBuffer;
+use arrow_array::builder::BooleanBufferBuilder;
+use arrow_buffer::{Buffer, buffer::NullBuffer};
 use arrow_data::{ArrayData, ArrayDataBuilder};
 use arrow_schema::{ArrowError, DataType};
 use std::marker::PhantomData;
@@ -61,11 +61,8 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ListLikeArrayDecoder<O, IS_VIEW> {
 impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDecoder<O, IS_VIEW> {
     fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
         let mut child_pos = Vec::with_capacity(pos.len());
-        let mut offsets = BufferBuilder::<O>::new(pos.len() + usize::from(!IS_VIEW));
-        if !IS_VIEW {
-            offsets.append(O::from_usize(0).unwrap());
-        }
-        let mut sizes = IS_VIEW.then(|| BufferBuilder::<O>::new(pos.len()));
+        let mut offsets = Vec::with_capacity(pos.len() + 1);
+        offsets.push(O::from_usize(0).unwrap());
 
         let mut nulls = self
             .is_nullable
@@ -85,14 +82,6 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDeco
                 _ => return Err(tape.error(*p, "[")),
             };
 
-            let start_idx = child_pos.len();
-            if IS_VIEW {
-                let offset = O::from_usize(start_idx).ok_or_else(|| {
-                    ArrowError::JsonError(format!("offset overflow decoding {}", self.data_type))
-                })?;
-                offsets.append(offset);
-            }
-
             let mut cur_idx = *p + 1;
             while cur_idx < end_idx {
                 child_pos.push(cur_idx);
@@ -101,17 +90,10 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDeco
                 cur_idx = tape.next(cur_idx, "list value")?;
             }
 
-            if IS_VIEW {
-                let size = O::from_usize(child_pos.len() - start_idx).ok_or_else(|| {
-                    ArrowError::JsonError(format!("size overflow decoding {}", self.data_type))
-                })?;
-                sizes.as_mut().unwrap().append(size);
-            } else {
-                let offset = O::from_usize(child_pos.len()).ok_or_else(|| {
-                    ArrowError::JsonError(format!("offset overflow decoding {}", self.data_type))
-                })?;
-                offsets.append(offset);
-            }
+            let offset = O::from_usize(child_pos.len()).ok_or_else(|| {
+                ArrowError::JsonError(format!("offset overflow decoding {}", self.data_type))
+            })?;
+            offsets.push(offset);
         }
 
         let child_data = self.decoder.decode(tape, &child_pos)?;
@@ -120,11 +102,19 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDeco
         let mut data = ArrayDataBuilder::new(self.data_type.clone())
             .len(pos.len())
             .nulls(nulls)
-            .add_buffer(offsets.finish())
             .child_data(vec![child_data]);
 
-        if let Some(mut sizes) = sizes {
-            data = data.add_buffer(sizes.finish());
+        if IS_VIEW {
+            let mut sizes = Vec::with_capacity(offsets.len() - 1);
+            for i in 1..offsets.len() {
+                sizes.push(offsets[i] - offsets[i - 1]);
+            }
+            offsets.pop();
+            data = data
+                .add_buffer(Buffer::from_vec(offsets))
+                .add_buffer(Buffer::from_vec(sizes));
+        } else {
+            data = data.add_buffer(Buffer::from_vec(offsets));
         }
 
         // Safety
