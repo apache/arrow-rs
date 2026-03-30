6e010138422cd50aaf175bd8e6ef7c57c50ab59d
Adam Gutglick <adam@spiraldb.com>
2026-03-30 15:19:26 +0100

    Run miri with nextest

--- .github/workflows/miri.sh
+++ .github/workflows/miri.sh
@@ -12,9 +12,9 @@ cargo miri setup
 cargo clean
 
 echo "Starting Arrow MIRI run..."
-cargo miri test -p arrow-buffer
-cargo miri test -p arrow-data --features ffi
-cargo miri test -p arrow-schema --features ffi
-cargo miri test -p arrow-ord
-cargo miri test -p arrow-array
-cargo miri test -p arrow-arith
+cargo miri nextest run -p arrow-buffer
+cargo miri nextest run -p arrow-data --features ffi
+cargo miri nextest run -p arrow-schema --features ffi
+cargo miri nextest run -p arrow-ord
+cargo miri nextest run -p arrow-array
+cargo miri nextest run -p arrow-arith
