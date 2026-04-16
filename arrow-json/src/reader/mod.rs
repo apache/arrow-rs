apache/arrow-rs

commit 07a363632bb57a1cab74b31ce4c67c8ec9814ebd

[Json] Replace `ArrayData` with typed Array construction in json-reader (#9497)
# Which issue does this PR close?

<!--
We generally require a GitHub issue to be filed for all bug fixes and
enhancements and this helps us generate change logs for our releases.
You can link an issue to this PR using the GitHub syntax.
-->

- Part of #9298.

# Rationale for this change

<!--
Why are you proposing this change? If this is already explained clearly
in the issue then this section is not needed.
Explaining clearly why changes are proposed helps reviewers understand
your changes and offer better suggestions for fixes.
-->

While implementing `ListViewArrayDecoder` in arrow-json, I noticed we
could potentially retire `ArrayDataBuilder` inside `ListArrayDecoder`.
Therefore, I'd like to use a small PR here to make sure there's no
regression

# What changes are included in this PR?

<!--
There is no need to duplicate the description in the issue here but it
is sometimes worth providing a summary of the individual changes in this
PR.
-->

Replace `ArrayDataBuilder` with `GenericListArray` in `ListArrayDecoder`

# Are these changes tested?

<!--
We typically require tests for all PRs in order to:
1. Prevent the code from being accidentally broken by subsequent changes
2. Serve as another way to document the expected behavior of the code

If tests are not included in your PR, please explain why (for example,
are they covered by existing tests)?
-->

Covered by existing tests

# Are there any user-facing changes?

<!--
If there are user-facing changes then we may require documentation to be
updated before approving the PR.

If there are any breaking changes to public APIs, please call them out.
-->

No

arrow-json/src/reader/mod.rs
@@ -133,25 +133,22 @@
 //! ```
 //!
 
-use crate::StructMode;
-use crate::reader::binary_array::{
-    BinaryArrayDecoder, BinaryViewDecoder, FixedSizeBinaryArrayDecoder,
-};
 use std::borrow::Cow;
 use std::io::BufRead;
 use std::sync::Arc;
 
+use arrow_array::cast::AsArray;
+use arrow_array::timezone::Tz;
+use arrow_array::types::*;
+use arrow_array::{ArrayRef, RecordBatch, RecordBatchReader, downcast_integer};
+use arrow_schema::{ArrowError, DataType, FieldRef, Schema, SchemaRef, TimeUnit};
 use chrono::Utc;
 use serde_core::Serialize;
 
-use arrow_array::timezone::Tz;
-use arrow_array::types::*;
-use arrow_array::{RecordBatch, RecordBatchReader, StructArray, downcast_integer, make_array};
-use arrow_data::ArrayData;
-use arrow_schema::{ArrowError, DataType, FieldRef, Schema, SchemaRef, TimeUnit};
-pub use schema::*;
-pub use value_iter::ValueIter;
-
+use crate::StructMode;
+use crate::reader::binary_array::{
+    BinaryArrayDecoder, BinaryViewDecoder, FixedSizeBinaryArrayDecoder,
+};
 use crate::reader::boolean_array::BooleanArrayDecoder;
 use crate::reader::decimal_array::DecimalArrayDecoder;
 use crate::reader::list_array::{ListArrayDecoder, ListViewArrayDecoder};
@@ -164,6 +161,9 @@ use crate::reader::string_view_array::StringViewArrayDecoder;
 use crate::reader::struct_array::StructArrayDecoder;
 use crate::reader::tape::{Tape, TapeDecoder};
 use crate::reader::timestamp_array::TimestampArrayDecoder;
+
+pub use schema::*;
+pub use value_iter::ValueIter;
 
 mod binary_array;
 mod boolean_array;
@@ -669,9 +669,9 @@ impl Decoder {
         self.tape_decoder.clear();
 
         let batch = match self.is_field {
-            true => RecordBatch::try_new(self.schema.clone(), vec![make_array(decoded)])?,
+            true => RecordBatch::try_new(self.schema.clone(), vec![decoded])?,
             false => {
-                RecordBatch::from(StructArray::from(decoded)).with_schema(self.schema.clone())?
+                RecordBatch::from(decoded.as_struct().clone()).with_schema(self.schema.clone())?
             }
         };
 
@@ -681,7 +681,7 @@ impl Decoder {
 
 trait ArrayDecoder: Send {
     /// Decode elements from `tape` starting at the indexes contained in `pos`
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError>;
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError>;
 }
 
 /// Context for decoder creation, containing configuration.
@@ -819,7 +819,7 @@ mod tests {
     use arrow_array::cast::AsArray;
     use arrow_array::{
         Array, BooleanArray, Float64Array, GenericListViewArray, ListArray, OffsetSizeTrait,
-        StringArray, StringViewArray,
+        StringArray, StringViewArray, StructArray, make_array,
     };
     use arrow_buffer::{ArrowNativeType, Buffer};
     use arrow_cast::display::{ArrayFormatter, FormatOptions};

arrow-json/src/reader/binary_array.rs
@@ -15,12 +15,13 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use arrow_array::builder::{BinaryViewBuilder, FixedSizeBinaryBuilder, GenericBinaryBuilder};
-use arrow_array::{Array, GenericStringArray, OffsetSizeTrait};
-use arrow_data::ArrayData;
-use arrow_schema::ArrowError;
 use std::io::Write;
 use std::marker::PhantomData;
+use std::sync::Arc;
+
+use arrow_array::builder::{BinaryViewBuilder, FixedSizeBinaryBuilder, GenericBinaryBuilder};
+use arrow_array::{ArrayRef, GenericStringArray, OffsetSizeTrait};
+use arrow_schema::ArrowError;
 
 use crate::reader::ArrayDecoder;
 use crate::reader::tape::{Tape, TapeElement};
@@ -87,7 +88,7 @@ pub struct BinaryArrayDecoder<O: OffsetSizeTrait> {
 }
 
 impl<O: OffsetSizeTrait> ArrayDecoder for BinaryArrayDecoder<O> {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let data_capacity = estimate_data_capacity(tape, pos)?;
 
         if O::from_usize(data_capacity).is_none() {
@@ -113,7 +114,7 @@ impl<O: OffsetSizeTrait> ArrayDecoder for BinaryArrayDecoder<O> {
             }
         }
 
-        Ok(builder.finish().into_data())
+        Ok(Arc::new(builder.finish()))
     }
 }
 
@@ -129,7 +130,7 @@ impl FixedSizeBinaryArrayDecoder {
 }
 
 impl ArrayDecoder for FixedSizeBinaryArrayDecoder {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let mut builder = FixedSizeBinaryBuilder::with_capacity(pos.len(), self.len);
         // Preallocate for the decoded byte width (FixedSizeBinary len), not the hex string length.
         let mut scratch = Vec::with_capacity(self.len as usize);
@@ -148,7 +149,7 @@ impl ArrayDecoder for FixedSizeBinaryArrayDecoder {
             }
         }
 
-        Ok(builder.finish().into_data())
+        Ok(Arc::new(builder.finish()))
     }
 }
 
@@ -156,7 +157,7 @@ impl ArrayDecoder for FixedSizeBinaryArrayDecoder {
 pub struct BinaryViewDecoder {}
 
 impl ArrayDecoder for BinaryViewDecoder {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let data_capacity = estimate_data_capacity(tape, pos)?;
         let mut builder = BinaryViewBuilder::with_capacity(data_capacity);
         let mut scratch = Vec::new();
@@ -175,7 +176,7 @@ impl ArrayDecoder for BinaryViewDecoder {
             }
         }
 
-        Ok(builder.finish().into_data())
+        Ok(Arc::new(builder.finish()))
     }
 }
 

arrow-json/src/reader/boolean_array.rs
@@ -15,9 +15,10 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use arrow_array::Array;
+use std::sync::Arc;
+
+use arrow_array::ArrayRef;
 use arrow_array::builder::BooleanBuilder;
-use arrow_data::ArrayData;
 use arrow_schema::ArrowError;
 
 use crate::reader::ArrayDecoder;
@@ -27,7 +28,7 @@ use crate::reader::tape::{Tape, TapeElement};
 pub struct BooleanArrayDecoder {}
 
 impl ArrayDecoder for BooleanArrayDecoder {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let mut builder = BooleanBuilder::with_capacity(pos.len());
         for p in pos {
             match tape.get(*p) {
@@ -38,6 +39,6 @@ impl ArrayDecoder for BooleanArrayDecoder {
             }
         }
 
-        Ok(builder.finish().into_data())
+        Ok(Arc::new(builder.finish()))
     }
 }

arrow-json/src/reader/decimal_array.rs
@@ -16,12 +16,12 @@
 // under the License.
 
 use std::marker::PhantomData;
+use std::sync::Arc;
 
-use arrow_array::Array;
+use arrow_array::ArrayRef;
 use arrow_array::builder::PrimitiveBuilder;
 use arrow_array::types::DecimalType;
 use arrow_cast::parse::parse_decimal;
-use arrow_data::ArrayData;
 use arrow_schema::ArrowError;
 
 use crate::reader::ArrayDecoder;
@@ -48,7 +48,7 @@ impl<D> ArrayDecoder for DecimalArrayDecoder<D>
 where
     D: DecimalType,
 {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let mut builder = PrimitiveBuilder::<D>::with_capacity(pos.len());
 
         for p in pos {
@@ -94,9 +94,10 @@ where
             }
         }
 
-        Ok(builder
-            .finish()
-            .with_precision_and_scale(self.precision, self.scale)?
-            .into_data())
+        Ok(Arc::new(
+            builder
+                .finish()
+                .with_precision_and_scale(self.precision, self.scale)?,
+        ))
     }
 }

arrow-json/src/reader/list_array.rs
@@ -15,20 +15,24 @@
 // specific language governing permissions and limitations
 // under the License.
 
+use std::marker::PhantomData;
+use std::sync::Arc;
+
+use arrow_array::builder::BooleanBufferBuilder;
+use arrow_array::{ArrayRef, GenericListArray, OffsetSizeTrait, make_array};
+use arrow_buffer::buffer::NullBuffer;
+use arrow_buffer::{Buffer, OffsetBuffer, ScalarBuffer};
+use arrow_data::ArrayDataBuilder;
+use arrow_schema::{ArrowError, DataType, FieldRef};
+
 use crate::reader::tape::{Tape, TapeElement};
 use crate::reader::{ArrayDecoder, DecoderContext};
-use arrow_array::OffsetSizeTrait;
-use arrow_array::builder::BooleanBufferBuilder;
-use arrow_buffer::{Buffer, buffer::NullBuffer};
-use arrow_data::{ArrayData, ArrayDataBuilder};
-use arrow_schema::{ArrowError, DataType};
-use std::marker::PhantomData;
 
 pub type ListArrayDecoder<O> = ListLikeArrayDecoder<O, false>;
 pub type ListViewArrayDecoder<O> = ListLikeArrayDecoder<O, true>;
 
 pub struct ListLikeArrayDecoder<O, const IS_VIEW: bool> {
-    data_type: DataType,
+    field: FieldRef,
     decoder: Box<dyn ArrayDecoder>,
     phantom: PhantomData<O>,
     is_nullable: bool,
@@ -50,7 +54,7 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ListLikeArrayDecoder<O, IS_VIEW> {
         let decoder = ctx.make_decoder(field.data_type(), field.is_nullable())?;
 
         Ok(Self {
-            data_type: data_type.clone(),
+            field: field.clone(),
             decoder,
             phantom: Default::default(),
             is_nullable,
@@ -59,7 +63,7 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ListLikeArrayDecoder<O, IS_VIEW> {
 }
 
 impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDecoder<O, IS_VIEW> {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let mut child_pos = Vec::with_capacity(pos.len());
         let mut offsets = Vec::with_capacity(pos.len() + 1);
         offsets.push(O::from_usize(0).unwrap());
@@ -91,18 +95,13 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDeco
             }
 
             let offset = O::from_usize(child_pos.len()).ok_or_else(|| {
-                ArrowError::JsonError(format!("offset overflow decoding {}", self.data_type))
+                ArrowError::JsonError(format!("offset overflow decoding {}ListArray", O::PREFIX))
             })?;
             offsets.push(offset);
         }
 
-        let child_data = self.decoder.decode(tape, &child_pos)?;
+        let values = self.decoder.decode(tape, &child_pos)?;
         let nulls = nulls.as_mut().map(|x| NullBuffer::new(x.finish()));
-
-        let mut data = ArrayDataBuilder::new(self.data_type.clone())
-            .len(pos.len())
-            .nulls(nulls)
-            .child_data(vec![child_data]);
 
         if IS_VIEW {
             let mut sizes = Vec::with_capacity(offsets.len() - 1);
@@ -110,15 +109,28 @@ impl<O: OffsetSizeTrait, const IS_VIEW: bool> ArrayDecoder for ListLikeArrayDeco
                 sizes.push(offsets[i] - offsets[i - 1]);
             }
             offsets.pop();
-            data = data
-                .add_buffer(Buffer::from_vec(offsets))
-                .add_buffer(Buffer::from_vec(sizes));
+            let data_type = if O::IS_LARGE {
+                DataType::LargeListView(self.field.clone())
+            } else {
+                DataType::ListView(self.field.clone())
+            };
+            // SAFETY: offsets and sizes are constructed correctly from the tape
+            let array_data = unsafe {
+                ArrayDataBuilder::new(data_type)
+                    .len(pos.len())
+                    .nulls(nulls)
+                    .child_data(vec![values.to_data()])
+                    .add_buffer(Buffer::from_vec(offsets))
+                    .add_buffer(Buffer::from_vec(sizes))
+                    .build_unchecked()
+            };
+            Ok(make_array(array_data))
         } else {
-            data = data.add_buffer(Buffer::from_vec(offsets));
-        }
+            // SAFETY: offsets are built monotonically starting from 0
+            let offsets = unsafe { OffsetBuffer::<O>::new_unchecked(ScalarBuffer::from(offsets)) };
 
-        // Safety
-        // Validated lengths above
-        Ok(unsafe { data.build_unchecked() })
+            let array = GenericListArray::<O>::try_new(self.field.clone(), offsets, values, nulls)?;
+            Ok(Arc::new(array))
+        }
     }
 }

arrow-json/src/reader/map_array.rs
@@ -15,16 +15,21 @@
 // specific language governing permissions and limitations
 // under the License.
 
+use std::sync::Arc;
+
+use arrow_array::builder::{BooleanBufferBuilder, BufferBuilder};
+use arrow_array::{ArrayRef, MapArray, StructArray};
+use arrow_buffer::buffer::NullBuffer;
+use arrow_buffer::{ArrowNativeType, OffsetBuffer, ScalarBuffer};
+use arrow_schema::{ArrowError, DataType, FieldRef, Fields};
+
 use crate::reader::tape::{Tape, TapeElement};
 use crate::reader::{ArrayDecoder, DecoderContext};
-use arrow_array::builder::{BooleanBufferBuilder, BufferBuilder};
-use arrow_buffer::ArrowNativeType;
-use arrow_buffer::buffer::NullBuffer;
-use arrow_data::{ArrayData, ArrayDataBuilder};
-use arrow_schema::{ArrowError, DataType};
 
 pub struct MapArrayDecoder {
-    data_type: DataType,
+    entries_field: FieldRef,
+    key_value_fields: Fields,
+    ordered: bool,
     keys: Box<dyn ArrayDecoder>,
     values: Box<dyn ArrayDecoder>,
     is_nullable: bool,
@@ -36,28 +41,38 @@ impl MapArrayDecoder {
         data_type: &DataType,
         is_nullable: bool,
     ) -> Result<Self, ArrowError> {
-        let fields = match data_type {
+        let (entries_field, ordered) = match data_type {
             DataType::Map(_, true) => {
                 return Err(ArrowError::NotYetImplemented(
                     "Decoding MapArray with sorted fields".to_string(),
                 ));
             }
-            DataType::Map(f, _) => match f.data_type() {
-                DataType::Struct(fields) if fields.len() == 2 => fields,
-                d => {
-                    return Err(ArrowError::InvalidArgumentError(format!(
-                        "MapArray must contain struct with two fields, got {d}"
-                    )));
-                }
-            },
+            DataType::Map(f, ordered) => (f.clone(), *ordered),
             _ => unreachable!(),
         };
 
-        let keys = ctx.make_decoder(fields[0].data_type(), fields[0].is_nullable())?;
-        let values = ctx.make_decoder(fields[1].data_type(), fields[1].is_nullable())?;
+        let key_value_fields = match entries_field.data_type() {
+            DataType::Struct(fields) if fields.len() == 2 => fields.clone(),
+            d => {
+                return Err(ArrowError::InvalidArgumentError(format!(
+                    "MapArray must contain struct with two fields, got {d}"
+                )));
+            }
+        };
+
+        let keys = ctx.make_decoder(
+            key_value_fields[0].data_type(),
+            key_value_fields[0].is_nullable(),
+        )?;
+        let values = ctx.make_decoder(
+            key_value_fields[1].data_type(),
+            key_value_fields[1].is_nullable(),
+        )?;
 
         Ok(Self {
-            data_type: data_type.clone(),
+            entries_field,
+            key_value_fields,
+            ordered,
             keys,
             values,
             is_nullable,
@@ -66,15 +81,7 @@ impl MapArrayDecoder {
 }
 
 impl ArrayDecoder for MapArrayDecoder {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
-        let s = match &self.data_type {
-            DataType::Map(f, _) => match f.data_type() {
-                s @ DataType::Struct(_) => s,
-                _ => unreachable!(),
-            },
-            _ => unreachable!(),
-        };
-
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let mut offsets = BufferBuilder::<i32>::new(pos.len() + 1);
         offsets.append(0);
 
@@ -110,34 +117,37 @@ impl ArrayDecoder for MapArrayDecoder {
             }
 
             let offset = i32::from_usize(key_pos.len()).ok_or_else(|| {
-                ArrowError::JsonError(format!("offset overflow decoding {}", self.data_type))
+                ArrowError::JsonError("offset overflow decoding MapArray".to_string())
             })?;
             offsets.append(offset)
         }
 
         assert_eq!(key_pos.len(), value_pos.len());
 
-        let key_data = self.keys.decode(tape, &key_pos)?;
-        let value_data = self.values.decode(tape, &value_pos)?;
+        let key_array = self.keys.decode(tape, &key_pos)?;
+        let value_array = self.values.decode(tape, &value_pos)?;
 
-        let struct_data = ArrayDataBuilder::new(s.clone())
-            .len(key_pos.len())
-            .child_data(vec![key_data, value_data]);
-
-        // Safety:
-        // Valid by construction
-        let struct_data = unsafe { struct_data.build_unchecked() };
+        // SAFETY: fields/arrays match the schema, lengths are equal, no nulls
+        let entries = unsafe {
+            StructArray::new_unchecked_with_length(
+                self.key_value_fields.clone(),
+                vec![key_array, value_array],
+                None,
+                key_pos.len(),
+            )
+        };
 
         let nulls = nulls.as_mut().map(|x| NullBuffer::new(x.finish()));
+        // SAFETY: offsets are built monotonically starting from 0
+        let offsets = unsafe { OffsetBuffer::new_unchecked(ScalarBuffer::from(offsets.finish())) };
 
-        let builder = ArrayDataBuilder::new(self.data_type.clone())
-            .len(pos.len())
-            .buffers(vec![offsets.finish()])
-            .nulls(nulls)
-            .child_data(vec![struct_data]);
-
-        // Safety:
-        // Valid by construction
-        Ok(unsafe { builder.build_unchecked() })
+        let array = MapArray::try_new(
+            self.entries_field.clone(),
+            offsets,
+            entries,
+            nulls,
+            self.ordered,
+        )?;
+        Ok(Arc::new(array))
     }
 }

arrow-json/src/reader/null_array.rs
@@ -15,21 +15,24 @@
 // specific language governing permissions and limitations
 // under the License.
 
+use std::sync::Arc;
+
+use arrow_array::{ArrayRef, NullArray};
+use arrow_schema::ArrowError;
+
 use crate::reader::ArrayDecoder;
 use crate::reader::tape::{Tape, TapeElement};
-use arrow_data::{ArrayData, ArrayDataBuilder};
-use arrow_schema::{ArrowError, DataType};
 
 #[derive(Default)]
 pub struct NullArrayDecoder {}
 
 impl ArrayDecoder for NullArrayDecoder {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         for p in pos {
             if !matches!(tape.get(*p), TapeElement::Null) {
                 return Err(tape.error(*p, "null"));
             }
         }
-        ArrayDataBuilder::new(DataType::Null).len(pos.len()).build()
+        Ok(Arc::new(NullArray::new(pos.len())))
     }
 }

arrow-json/src/reader/primitive_array.rs
@@ -15,15 +15,15 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use num_traits::NumCast;
 use std::marker::PhantomData;
+use std::sync::Arc;
 
 use arrow_array::builder::PrimitiveBuilder;
-use arrow_array::{Array, ArrowPrimitiveType};
+use arrow_array::{ArrayRef, ArrowPrimitiveType};
 use arrow_cast::parse::Parser;
-use arrow_data::ArrayData;
 use arrow_schema::{ArrowError, DataType};
 use half::f16;
+use num_traits::NumCast;
 
 use crate::reader::ArrayDecoder;
 use crate::reader::tape::{Tape, TapeElement};
@@ -93,7 +93,7 @@ where
     P: ArrowPrimitiveType + Parser,
     P::Native: ParseJsonNumber + NumCast,
 {
-    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayData, ArrowError> {
+    fn decode(&mut self, tape: &Tape<'_>, pos: &[u32]) -> Result<ArrayRef, ArrowError> {
         let mut builder =
             PrimitiveBuilder::<P>::with_capacity(pos.len()).with_data_type(self.data_type.clone());
         let d = &self.data_type;
@@ -154,6 +154,6 @@ where
             }
         }
 
-        Ok(builder.finish().into_data())
+        Ok(Arc::new(builder.finish()))
     }
 }

commit ddde66de9139af42448d775cc2b49c77570d2386

Add a test for reading nested REE data in json (#9634)
# Which issue does this PR close?

- related to https://github.com/apache/arrow-rs/pull/9497

# Rationale for this change

I (well Codex) found a regression in the changes in
https://github.com/apache/arrow-rs/pull/9497 but they are not covered by
a test. So we should add a test

# What changes are included in this PR?

Add a test for reading nested REE data from json (that currently passes
on main but fails on https://github.com/apache/arrow-rs/pull/9497)

# Are these changes tested?

Only tests

# Are there any user-facing changes?

No this is just a test

Co-authored-by: Jeffrey Vo <jeffrey.vo.australia@gmail.com>

arrow-json/src/reader/mod.rs
@@ -3457,4 +3457,43 @@ mod tests {
         assert_eq!(run_array.len(), 3);
         assert_eq!(run_array.run_ends().values(), &[2i16, 3]);
     }
+
+    #[test]
+    fn test_read_nested_run_end_encoded() {
+        let buf = r#"
+        {"a": "x"}
+        {"a": "x"}
+        {"a": "y"}
+        "#;
+
+        // The outer REE compresses whole rows, while the inner REE compresses the
+        // repeated string values produced by decoding those rows.
+        let inner_type = DataType::RunEndEncoded(
+            Arc::new(Field::new("run_ends", DataType::Int64, false)),
+            Arc::new(Field::new("values", DataType::Utf8, true)),
+        );
+        let outer_type = DataType::RunEndEncoded(
+            Arc::new(Field::new("run_ends", DataType::Int64, false)),
+            Arc::new(Field::new("values", inner_type, true)),
+        );
+        let schema = Arc::new(Schema::new(vec![Field::new("a", outer_type, true)]));
+        let batches = do_read(buf, 1024, false, false, schema);
+        assert_eq!(batches.len(), 1);
+
+        let col = batches[0].column(0);
+        let outer = col.as_run::<arrow_array::types::Int64Type>();
+        // Three logical rows compress to two outer runs: ["x", "x"] and ["y"].
+        assert_eq!(outer.len(), 3);
+        assert_eq!(outer.run_ends().values(), &[2, 3]);
+
+        let nested = outer.values().as_run::<arrow_array::types::Int64Type>();
+        // The physical values of the outer REE are themselves a two-element REE.
+        assert_eq!(nested.len(), 2);
+        assert_eq!(nested.run_ends().values(), &[1, 2]);
+
+        let nested_values = nested.values().as_string::<i32>();
+        assert_eq!(nested_values.len(), 2);
+        assert_eq!(nested_values.value(0), "x");
+        assert_eq!(nested_values.value(1), "y");
+    }
 }