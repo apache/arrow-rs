b0a5fb1d459ea0ec09bc87b9b13633363a5d1d1d
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2026-06-11 14:29:54 +0300

    feat(buffer): allow `Buffer::into_mutable` to return for sliced buffer from the middle and add `Buffer::is_sliced` helper
    before, `Buffer::into_mutable` would panic if you pass it `Buffer` that
    was offseted from the underlying bytes, but it would be ok for sliced
    `Buffer` that start at the same position but ends before the data itself

--- arrow-buffer/src/buffer/immutable.rs
+++ arrow-buffer/src/buffer/immutable.rs
@@ -113,9 +113,9 @@ impl Buffer {
         unsafe { self.ptr.offset_from(self.data.ptr().as_ptr()) as usize }
     }
 
-    /// Returns whether the buffer is sliced
+    /// Returns whether the buffer is sliced (does not point to entire original data) 
     pub fn is_sliced(&self) -> bool {
-        self.ptr != self.data.as_ptr() || self.length != self.data.len()
+        self.length != self.data.len()
     }
 
     /// Returns the pointer to the start of the buffer without the offset.
@@ -369,9 +369,10 @@ impl Buffer {
     /// Returns `Err` if this is shared or its allocation is from an external source or
     /// it is not allocated with alignment [`ALIGNMENT`]
     ///
-    /// If the buffer is sliced, the returned [`MutableBuffer`] will be a view of the original buffer.
+    /// If the buffer is sliced, the returned [`MutableBuffer`] will be a view of the original buffer
+    /// (include values outside the sliced data).
     ///
-    /// you can check if the buffer is sliced by
+    /// you can check if the buffer is sliced by calling [`Self::is_sliced`]
     ///
     /// # Example: Creating a [`MutableBuffer`] from a [`Buffer`]
     /// ```
@@ -1094,17 +1095,49 @@ mod tests {
         assert_eq!(buffer2.strong_count(), 1);
     }
 
-    // test cases
-    // | offset | ptr_length | data_length | result |
-    // | ------ | ---------- | ----------- | ------ |
-    // | 0      | 10         | 10          | false  |
-    // | 0      | 8          | 10          | true   |
-    // | 1      | 8          | 10          | true   |
-    // | 1      | 9          | 10          | true   |
-    // 
-    // 
     #[test]
-    fn test_is_sliced_should_return_false() {
-
+    fn test_is_sliced() {
+      let buffer = Buffer::from(&[1, 2, 3, 4]);
+      assert!(!buffer.is_sliced());
+      assert!(!buffer.clone().is_sliced());
+      {
+        let mut advanced = buffer.clone();
+        advanced.advance(0);
+        assert!(!advanced.is_sliced());
+      }
+      {
+        let mut advanced = buffer.clone();
+        advanced.advance(1);
+        assert!(advanced.is_sliced());
+      }
+      
+      assert!(!buffer.slice(0).is_sliced());
+      assert!(buffer.slice(1).is_sliced());
+      
+      assert!(buffer.slice_with_length(1, 3).is_sliced());
+      assert!(!buffer.slice_with_length(0, 4).is_sliced());
+      assert!(buffer.slice_with_length(0, 3).is_sliced());
+      assert!(buffer.slice_with_length(0, 0).is_sliced());
+    }
+    
+    #[test]
+    fn into_mutable_should_return_the_entire_data_regardless_of_slicing() {
+      let original_buffer_data = [1_u8, 2, 3, 4, 5, 6, 7, 8];
+      for (slice_from, slice_length) in [(0, 0), (0, original_buffer_data.len()), (original_buffer_data.len(), 0), (2, 4), (2, original_buffer_data.len() - 2)] {
+        let buffer = Buffer::from(original_buffer_data.clone());
+        let original_buffer_len = buffer.len();
+        let original_data_ptr = buffer.data_ptr();
+        let sliced = buffer.slice_with_length(slice_from, slice_length);
+        drop(buffer); // Keep only 1 owner
+        
+        let mutable = sliced.into_mutable().expect("should convert to mutable");
+        assert_eq!(mutable.len(), original_buffer_len);
+        let new_buffer = Buffer::from(mutable);
+        assert_eq!(new_buffer.data_ptr(), original_data_ptr);
+        assert_eq!(new_buffer.len(), original_buffer_len);
+        assert!(!new_buffer.is_sliced());
+        
+        assert_eq!(new_buffer.as_slice(), &original_buffer_data);
+      }
     }
 }
