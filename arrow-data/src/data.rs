c79a3eeafd5fd9ae880c0c9a0fec5d6d08e9854b
Raz Luvaton <16746759+rluvaton@users.noreply.github.com>
2026-07-04 22:58:57 +0300

    add test

--- arrow-data/src/data.rs
+++ arrow-data/src/data.rs
@@ -2747,13 +2747,22 @@ mod tests {
     // Even when `force_validate` feature is on
     #[test]
     fn test_dont_panic_on_bad_input_when_using_try_new() {
-      let 
+        let empty_bytes = Buffer::default();
+
+        let builder = ArrayDataBuilder::new(DataType::Utf8)
+            .len(1)
+
+            // the offsets says that we have 2 bytes but the buffer is empty
+            .buffers(vec![Buffer::from_vec(vec![0i32, 2i32]), empty_bytes])
+            .nulls(None);
+
+      let res = builder.build().expect_err("should get error");
+      
 
       assert_eq!(
-          err.to_string(),
+          res.to_string(),
           format!(
-              "Invalid argument error: Length {} with offset 1 overflows usize for Binary",
-              usize::MAX
+              "Invalid argument error: Last offset 2 of Utf8 is larger than values length 0",
           )
       );
     }
