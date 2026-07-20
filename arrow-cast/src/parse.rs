534f92ef480ef4b09cde587f8de4ce3af259f652
rich-T-kid <richardbaah@MacBook-Air-de-Richard.local>
2026-07-20 16:38:26 -0400

    exist early for empty cases

--- arrow-cast/src/parse.rs
+++ arrow-cast/src/parse.rs
@@ -445,7 +445,7 @@ pub trait Parser: ArrowPrimitiveType {
 
 impl Parser for Float16Type {
     fn parse(string: &str) -> Option<f16> {
-        let (string, _) = trim_pre_and_post_whitespace(string);
+        let string = trim_pre_and_post_whitespace(string);
         lexical_core::parse(string.as_bytes())
             .ok()
             .map(f16::from_f32)
@@ -454,30 +454,29 @@ impl Parser for Float16Type {
 
 impl Parser for Float32Type {
     fn parse(string: &str) -> Option<f32> {
-        let (string, _) = trim_pre_and_post_whitespace(string);
+        let string = trim_pre_and_post_whitespace(string);
         lexical_core::parse(string.as_bytes()).ok()
     }
 }
 
 impl Parser for Float64Type {
     fn parse(string: &str) -> Option<f64> {
-        let (string, _) = trim_pre_and_post_whitespace(string);
+        let string = trim_pre_and_post_whitespace(string);
         lexical_core::parse(string.as_bytes()).ok()
     }
 }
 
 /// this is a no-op if the string starts and ends with a digit, otherwise it will trim whitespace from the start and end of the string.
 #[inline]
-fn trim_pre_and_post_whitespace(string: &str) -> (&str, bool) {
+fn trim_pre_and_post_whitespace(string: &str) -> &str {
     let bytes = string.as_bytes();
     let prefix = bytes.first().is_some_and(|b| !b.is_ascii_digit());
     let suffix = bytes.last().is_some_and(|b| !b.is_ascii_digit());
-    println!("bytes : {:?}, len: {}", bytes, bytes.len());
     match (prefix, suffix) {
-        (false, false) => (string, bytes.len() >= 1),
-        (true, false) => (string.trim_ascii_start(), false),
-        (false, true) => (string.trim_ascii_end(), false),
-        (true, true) => (string.trim_ascii(), false),
+        (false, false) => string,
+        (true, false) => string.trim_ascii_start(),
+        (false, true) => string.trim_ascii_end(),
+        (true, true) => string.trim_ascii(),
     }
 }
 
@@ -485,10 +484,10 @@ macro_rules! parser_primitive {
     ($t:ty) => {
         impl Parser for $t {
             fn parse(string: &str) -> Option<Self::Native> {
-                let (string, not_empty) = trim_pre_and_post_whitespace(string);
-                if !not_empty {
+                if string.is_empty() {
                     return None;
                 }
+                let string = trim_pre_and_post_whitespace(string);
                 match atoi::FromRadix10SignedChecked::from_radix_10_signed_checked(
                     string.as_bytes(),
                 ) {
