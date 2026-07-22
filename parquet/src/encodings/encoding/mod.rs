75d06a796a3d19bdd891749eb1559e4c6e3f4dac
Michal Piatkowski <291740709+MassivePizza@users.noreply.github.com>
2026-07-22 17:48:19 +0200

    fix empty ByteArray panic

--- parquet/src/encodings/encoding/mod.rs
+++ parquet/src/encodings/encoding/mod.rs
@@ -690,7 +690,7 @@ impl<T: DataType> DeltaByteArrayEncoder<T> {
             suffix_writer: DeltaLengthByteArrayEncoder::new(),
             prefix_lengths: Vec::new(),
             suffixes: Vec::new(),
-            previous: ByteArray::new(),
+            previous: Bytes::new().into(),
             _phantom: PhantomData,
         }
     }
@@ -749,7 +749,7 @@ impl<T: DataType> Encoder<T> for DeltaByteArrayEncoder<T> {
                 // ... followed by suffixes
                 self.suffix_writer.flush_to(out)?;
 
-                self.previous = ByteArray::new();
+                self.previous = Bytes::new().into();
                 Ok(())
             }
             _ => panic!(
