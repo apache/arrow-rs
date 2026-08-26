506fcb95937bd0618cde852835b7ec5d33038a3a
RIchard Baah <richard.baah@datadoghq.com>
2026-08-26 11:01:01 -0400

    trim comments

--- arrow-array/src/builder/boolean_builder.rs
+++ arrow-array/src/builder/boolean_builder.rs
@@ -167,7 +167,7 @@ impl BooleanBuilder {
             .add_buffer(self.values_builder.finish().into_inner())
             .nulls(null_bit_buffer);
 
-        // SAFETY: builder is constructed from valid boolean bitmap and null buffer with matching lengths
+        // SAFETY: values buffer and nulls have matching lengths
         let array_data = unsafe { builder.build_unchecked() };
         BooleanArray::from(array_data)
     }
@@ -182,7 +182,7 @@ impl BooleanBuilder {
             .add_buffer(value_buffer)
             .nulls(nulls);
 
-        // SAFETY: builder is constructed from valid boolean bitmap and null buffer with matching lengths
+        // SAFETY: values buffer and nulls have matching lengths
         let array_data = unsafe { builder.build_unchecked() };
         BooleanArray::from(array_data)
     }
