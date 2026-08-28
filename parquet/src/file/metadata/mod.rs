7a259cb735f1d5ac8132a083806d03a0df34ba75
Ed Seidl <etseidl@live.com>
2026-08-28 11:44:16 -0700

    describe the ranges better

--- parquet/src/file/metadata/mod.rs
+++ parquet/src/file/metadata/mod.rs
@@ -1348,6 +1348,9 @@ impl ColumnChunkMetaData {
     }
 
     /// Returns the range for the column index, if any.
+    ///
+    /// The range is `[column_index_offset, column_index_offset + column_index_length)`,
+    /// where the offset is relative to the start of the file.
     pub fn column_index_range(&self) -> Option<Range<u64>> {
         let offset = u64::try_from(self.column_index_offset?).ok()?;
         let length = u64::try_from(self.column_index_length?).ok()?;
@@ -1365,6 +1368,9 @@ impl ColumnChunkMetaData {
     }
 
     /// Returns the range for the offset index, if any.
+    ///
+    /// The range is `[offset_index_offset, offset_index_offset + offset_index_length)`,
+    /// where the offset is relative to the start of the file.
     pub fn offset_index_range(&self) -> Option<Range<u64>> {
         let offset = u64::try_from(self.offset_index_offset?).ok()?;
         let length = u64::try_from(self.offset_index_length?).ok()?;
