964ec1c3336bf5d8598c451975ec6df094685109
Jefffrey <jeffrey.vo.australia@gmail.com>
2026-09-02 11:35:12 +0900

    Remove `force_validate` feature from `arrow-cast`

--- arrow-cast/src/cast/mod.rs
+++ arrow-cast/src/cast/mod.rs
@@ -3027,7 +3027,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(feature = "force_validate"))]
     #[should_panic(
         expected = "Cannot cast to Decimal128(20, 3). Overflowing on 57896044618658097711785492504343953926634992332820282019728792003956564819967"
     )]
@@ -3064,7 +3063,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg(not(feature = "force_validate"))]
     fn test_cast_decimal_to_decimal_round() {
         let array = vec![
             Some(1123454),
