06a8bdddea4a439941009d79f7132113daa8a073
Andrew Lamb <andrew@nerdnetworks.org>
2026-09-08 16:28:47 -0400

    Update link to filed issue

--- parquet/src/file/metadata/mod.rs
+++ parquet/src/file/metadata/mod.rs
@@ -234,13 +234,10 @@ impl ParquetMetaData {
         #[cfg(not(feature = "encryption"))]
         let encryption_size = 0usize;
 
-        // We can only determine the heap size for PageIndex. Custom providers are
-        // out of scope.
-        // TODO: add heap_size calculation to the PageIndexProvider trait in some way.
-        // see https://github.com/apache/arrow-rs/pull/9138 and
-        // https://github.com/apache/arrow-rs/pull/9842 for some prior attempts. It
-        // would be nice for users to re-use the HeapSize machinery this crate already
-        // implements.
+        // We can only determine the heap size for PageIndex.
+        //
+        // Heap size for custom `PageIndexProvider` tracked by
+        // <https://github.com/apache/arrow-rs/issues/11028>
         let page_index_size = if let Some(page_index) = self.page_index.as_ref() {
             if let Some(page_index) = page_index.as_any().downcast_ref::<PageIndex>() {
                 // need to account for the `Arc` wrapper overhead
