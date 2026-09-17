152faa50f002b6a964a3b34512a15b07023d5c05
RIchard Baah <richard.baah@datadoghq.com>
2026-09-17 14:40:34 -0400

    bench(coalesce): add FixedSizeList and List benchmarks, extract FSB helper

--- arrow/benches/coalesce_kernels.rs
+++ arrow/benches/coalesce_kernels.rs
@@ -122,6 +122,18 @@ fn add_all_filter_benchmarks(c: &mut Criterion) {
         true,
     )]));
 
+    let single_fsl4_schema = SchemaRef::new(Schema::new(vec![Field::new(
+        "value",
+        DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Int32, true)), 4),
+        true,
+    )]));
+
+    let single_list_schema = SchemaRef::new(Schema::new(vec![Field::new(
+        "value",
+        DataType::List(Arc::new(Field::new("item", DataType::Int32, true))),
+        true,
+    )]));
+
     // Null density: 0, 10%
     for null_density in [0.0, 0.1] {
         // Selectivity: 0.1%, 1%, 10%, 80%
@@ -343,6 +355,30 @@ fn add_all_filter_benchmarks(c: &mut Criterion) {
                 schema: &single_boolean_schema,
             }
             .build();
+
+            FilterBenchmarkBuilder {
+                c,
+                name: "single_fsl4",
+                batch_size,
+                num_output_batches: 50,
+                null_density,
+                selectivity,
+                max_string_len: 0,
+                schema: &single_fsl4_schema,
+            }
+            .build();
+
+            FilterBenchmarkBuilder {
+                c,
+                name: "single_list",
+                batch_size,
+                num_output_batches: 50,
+                null_density,
+                selectivity,
+                max_string_len: 0,
+                schema: &single_list_schema,
+            }
+            .build();
         }
     }
 }
@@ -423,6 +459,18 @@ fn add_all_take_benchmarks(c: &mut Criterion) {
     let single_boolean_schema = SchemaRef::new(Schema::new(vec![Field::new(
         "value",
         DataType::Boolean,
+        true,
+    )]));
+
+    let single_fsl4_schema = SchemaRef::new(Schema::new(vec![Field::new(
+        "value",
+        DataType::FixedSizeList(Arc::new(Field::new("item", DataType::Int32, true)), 4),
+        true,
+    )]));
+
+    let single_list_schema = SchemaRef::new(Schema::new(vec![Field::new(
+        "value",
+        DataType::List(Arc::new(Field::new("item", DataType::Int32, true))),
         true,
     )]));
 
@@ -512,6 +560,18 @@ fn add_all_take_benchmarks(c: &mut Criterion) {
                     num_output_batches: 50,
                     max_string_len: 0,
                     schema: &single_boolean_schema,
+                },
+                TakeBenchmarkScenario {
+                    name: "single_fsl4",
+                    num_output_batches: 50,
+                    max_string_len: 0,
+                    schema: &single_fsl4_schema,
+                },
+                TakeBenchmarkScenario {
+                    name: "single_list",
+                    num_output_batches: 50,
+                    max_string_len: 0,
+                    schema: &single_list_schema,
                 },
             ] {
                 TakeBenchmarkBuilder::from_scenario(
@@ -1123,14 +1183,23 @@ impl DataStreamBuilder {
                 0.5,
                 seed,
             )),
-            DataType::FixedSizeList(_, list_size) => {
-                Arc::new(create_primitive_fixed_size_list_array::<Int32Type>(
+            DataType::FixedSizeList(_, list_size) => Arc::new(
+                create_primitive_fixed_size_list_array::<Int32Type>(
                     self.batch_size,
                     self.null_density,
                     0.0,
                     *list_size,
-                ))
-            }
+                ),
+            ),
+            DataType::List(_) => Arc::new(
+                create_primitive_list_array_with_seed::<i32, Int32Type>(
+                    self.batch_size,
+                    self.null_density,
+                    0.0,
+                    10,
+                    seed,
+                ),
+            ),
             _ => panic!("Unsupported data type: {field:?}"),
         }
     }
